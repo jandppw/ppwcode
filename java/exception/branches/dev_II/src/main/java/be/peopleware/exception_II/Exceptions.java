3a78415586a3b61ac557c96fbbffffc8f8aced5a
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-06-03 18:23:13 +0000

    Some changes to make things compile "more", using org.ppwcode.metainfo_I.

--- java/exception/branches/dev_II/src/main/java/be/peopleware/exception_II/Exceptions.java
+++ java/exception/branches/dev_II/src/main/java/be/peopleware/exception_II/Exceptions.java
@@ -1,14 +1,28 @@
 /*<license>
-  Copyright 2004, PeopleWare n.v.
-  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
-  TO SELECTED PARTIES.
+Copyright 2005 - $Date$ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 </license>*/
 
 package be.peopleware.exception_II;
 
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
 
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.el.ELException;
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+
+
 
 //import org.apache.commons.logging.Log;
 
@@ -19,6 +33,10 @@ import javax.servlet.jsp.el.ELException;
  * @author      Jan Dockx
  * @author      PeopleWare n.v.
  */
+@Copyright("2007 - $Date$, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
 public class Exceptions {
 
   /*<section name="Meta Information">*/
