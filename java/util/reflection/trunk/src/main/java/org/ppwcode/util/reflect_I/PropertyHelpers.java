57d73beb73e2f26ea6cedb85c2c068c5f3e77e11
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-07-28 17:49:08 +0000

    we don't need that import

--- java/util/reflection/trunk/src/main/java/org/ppwcode/util/reflect_I/PropertyHelpers.java
+++ java/util/reflection/trunk/src/main/java/org/ppwcode/util/reflect_I/PropertyHelpers.java
@@ -23,7 +23,6 @@ import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.beans.PropertyEditor;
 import java.beans.PropertyEditorManager;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
