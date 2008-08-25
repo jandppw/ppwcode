24a6ccc76eb2fe0ac627cbcd1d0f2ff8512f5b4a
ppw.bot <ppw.bot@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-08-25 18:35:04 +0000

    svn_spider [r1824]
    working root:
      https://ppwcode.googlecode.com/svn/java/util/reflection/trunk
    
    fixes:
      src/main/java/org/ppwcode/util/reflect_I/InstanceHelpers.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/util/reflect_I/PropertyHelpersTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/util/reflect_I/teststubs/CloneableStubClassAEditor.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source

--- java/util/reflection/trunk/src/main/java/org/ppwcode/util/reflect_I/InstanceHelpers.java
+++ java/util/reflection/trunk/src/main/java/org/ppwcode/util/reflect_I/InstanceHelpers.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2004 - $Date: 2008-07-31 01:17:04 +0200 (Thu, 31 Jul 2008) $ by PeopleWare n.v..
+Copyright 2004 - $Date$ by PeopleWare n.v..
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -34,10 +34,10 @@ import org.ppwcode.vernacular.exception_II.ProgrammingErrors;
  * @author    Jan Dockx
  * @author    PeopleWare n.v.
  */
-@Copyright("2004 - $Date: 2008-07-31 01:17:04 +0200 (Thu, 31 Jul 2008) $, PeopleWare n.v.")
+@Copyright("2004 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
-@SvnInfo(revision = "$Revision: 1978 $",
-         date     = "$Date: 2008-07-31 01:17:04 +0200 (Thu, 31 Jul 2008) $")
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
 public class InstanceHelpers {
 
   private InstanceHelpers() {
