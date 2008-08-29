e0b9be9f0c22687728e1483488ebc5efe53fd494
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-08-29 15:10:25 +0000

    opying tag structure from old ppw value work done in new ppwcode vernacular value project to other offshoot ppwcode value; shouldn't have been a mv, but a cp

---
+++ java/vernacular/value/II/1.n.n/1.3.n/II-1.3.0-1.2/src/java/be/peopleware/value_II/RSZNumberConverter.java
@@ -0,0 +1,61 @@
+package be.peopleware.value_II;
+
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+
+import be.peopleware.bean_V.PropertyException;
+
+
+/**
+ * Converter for {@link RSZNumber}. The {@link #getAsObject(FacesContext, UIComponent, String)}
+ * method is very lenient: separators can be left out, or any of the characters
+ * <code><var>space</var>-/|*:</code>.
+ *
+ * The value must be the string representation of the class.
+ *
+ * To activate this converter, the following entry has to appear in <kbd>faces-config.xml</kbd>:
+ * <pre>
+ * &lt;converter&gt;
+ *   &lt;converter-for-class&gt;be.peopleware.value_I.RSZNumber&lt;/converter-for-class&gt;
+ *   &lt;converter-class&gt;be.peopleware.value_I.RSZNumberConverter&lt;/converter-class&gt;
+ * &lt;/converter&gt;
+ * </pre>
+ *
+ * @author nsmeets
+ * @author Jan Dockx
+ * @author Peopleware n.v.
+ */
+public class RSZNumberConverter implements Converter {
+
+  public Object getAsObject(FacesContext context, UIComponent component, String value)
+      throws ConverterException {
+    if ((value == null) || (value.length() == 0)) {
+      return null;
+    }
+    else {
+      try {
+        return new RSZNumber(value);
+      }
+      catch (PropertyException e) {
+        throw new ConverterException("RSZ number " + value + "could not be created", e);
+      }
+    }
+  }
+
+  public final static String EMPTY = "";
+
+  public String getAsString(FacesContext context, UIComponent component, Object value)
+      throws ConverterException {
+    if (value == null) {
+      return EMPTY;
+    }
+    if (! (value instanceof RSZNumber)) {
+      throw new ConverterException("value is not a RSZNumber");
+    }
+    return ((RSZNumber)value).toString();
+  }
+}
+
