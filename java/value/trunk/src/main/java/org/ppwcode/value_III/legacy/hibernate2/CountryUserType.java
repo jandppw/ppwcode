c3d1ceb79ec73b5478df50881db12953d017f736
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-09-24 16:50:43 +0000

    Compiles

--- java/value/trunk/src/main/java/org/ppwcode/value_III/legacy/hibernate2/CountryUserType.java
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/legacy/hibernate2/CountryUserType.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 </license>*/
 
-package org.ppwcode.value_III.legacy;
+package org.ppwcode.value_III.legacy.hibernate2;
 
 
 import org.ppwcode.value_III.legacy.Country;
@@ -28,8 +28,10 @@ import org.ppwcode.vernacular.value_III.hibernate2.AbstractEnumerationUserType;
  *
  * @author    Jan Dockx
  * @author    Peopleware n.v.
+ *
+ * @deprecated
  */
-
+@Deprecated
 public final class CountryUserType extends AbstractEnumerationUserType {
 
   /*<section name="Meta Information">*/
