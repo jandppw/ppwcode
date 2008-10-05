86742bac81b457fecaf0272e2ef02c79ab3437a9
ppw.bot <ppw.bot@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-10-05 18:33:16 +0000

    svn_spider [r1824]
    working root:
      https://ppwcode.googlecode.com/svn/java/util/reflection/trunk
    
    fixes:
      src/main/java/org/ppwcode/util/reflect_I/AssociationHelpers.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/util/reflect_I/AssociationHelpersTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/util/reflect_I/teststubs/StubAssociatedBean.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/util/reflect_I/teststubs/StubAssociatedBeanA.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/util/reflect_I/teststubs/StubAssociatedBeanB.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source

--- java/util/reflection/trunk/src/test/java/org/ppwcode/util/reflect_I/AssociationHelpersTest.java
+++ java/util/reflection/trunk/src/test/java/org/ppwcode/util/reflect_I/AssociationHelpersTest.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2004 - $Date: 2008-07-31 01:22:10 +0200 (Thu, 31 Jul 2008) $ by PeopleWare n.v..
+Copyright 2004 - $Date$ by PeopleWare n.v..
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
