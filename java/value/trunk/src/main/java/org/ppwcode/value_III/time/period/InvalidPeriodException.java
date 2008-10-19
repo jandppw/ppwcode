eba8e1bc708bdee6aaa8465cea6df98f481c3f42
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-10-19 20:59:29 +0000

    period in separate package: there will be a lot of classes dealing with periods

--- java/value/trunk/src/main/java/org/ppwcode/value_III/time/period/InvalidPeriodException.java
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/time/period/InvalidPeriodException.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 </license>*/
 
-package org.ppwcode.value_III.time;
+package org.ppwcode.value_III.time.period;
 
 
 import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
