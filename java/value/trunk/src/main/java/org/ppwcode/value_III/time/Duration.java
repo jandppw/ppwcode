74ba7b9752e1d020855a16fc131493c26a4eac5f
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-10-27 10:26:29 +0000

    Clean, comments, contracts

--- java/value/trunk/src/main/java/org/ppwcode/value_III/time/Duration.java
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/time/Duration.java
@@ -19,6 +19,8 @@ package org.ppwcode.value_III.time;
 
 import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
 import static org.ppwcode.value_III.time.Duration.Unit.MILLISECOND;
+import static org.ppwcode.vernacular.exception_II.ProgrammingErrorHelpers.pre;
+import static org.ppwcode.vernacular.exception_II.ProgrammingErrorHelpers.preArgumentNotNull;
 
 import java.text.NumberFormat;
 
@@ -26,55 +28,111 @@ import org.ppwcode.metainfo_I.Copyright;
 import org.ppwcode.metainfo_I.License;
 import org.ppwcode.metainfo_I.vcs.SvnInfo;
 import org.ppwcode.vernacular.value_III.AbstractImmutableValue;
+import org.toryt.annotations_I.Basic;
+import org.toryt.annotations_I.Expression;
+import org.toryt.annotations_I.Invars;
+import org.toryt.annotations_I.MethodContract;
 
 
 /**
- * A period has a start date and an end date.
- * The start date of a period is strictly before the end date (so a period
- * should not be empty).
- * The start date is included in the interval, the end date is not
- * (right half-open interval).
- *
- * The {@link #compareTo(Object) compare method} compares the
- * {@link #getBegin()}.
- *
- * @author    nsmeets
- * @author    Peopleware NV
- *
- * @invar     (getStartDate() != null && getEndDate() != null)
- *                ? getStartDate().before(getEndDate())
- *                : true;
- *
- * @mudo (jand) must be a value, and move to ppw-value; suggest mutable; (add
- *       normalize method and getWildExceptions ?)
- * @mudo (nsmeets) Normalization does not seem to be a good idea.
- *
- * @see http://en.wikipedia.org/wiki/Allen's_Interval_Algebra Allen's Interval Algebra
+ * A duration of time. The smallest unit we can work with are <em>ms</em>.
  */
 @Copyright("2008 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
 @SvnInfo(revision = "$Revision$",
          date     = "$Date$")
-public class Duration extends AbstractImmutableValue {
+public final class Duration extends AbstractImmutableValue implements Comparable<Duration> {
 
   public static enum Unit {
-    MILLISECOND (1),
-    SECOND      (1000),
-    MINUTE      (60 * 1000),
-    HOUR        (60 * 60 * 1000),
-    DAY         (24 * 60 * 60 * 1000),
-    WEEK        (7 * 24 * 60 * 60 * 1000),
-    MONTH       (30 * 24 * 60 * 60 * 1000),
-    QUARTER     (3 * 30 * 24 * 60 * 60 * 1000),
-    YEAR        (365 * 24 * 60 * 60 * 1000),
-    DECENNIUM   (((10 * 365) + 2) * 24 * 60 * 60 * 1000),
-    CENTURY     (((100 * 365) + 24) * 24 * 60 * 60 * 1000),
-    MILENNIUM   (((1000 * 365) + 241) * 24 * 60 * 60 * 1000);
 
-    Unit(int milliseconds) {
+    /**
+     * The smallest duration unit we support.
+     * (There are ways to support more units. Please implement them and submit your patch.)
+     */
+    MILLISECOND (1),
+
+    @Invars(@Expression("SECOND.asMilliseconds() == 1000"))
+    SECOND      (1000),
+
+    @Invars(@Expression("MINUTE.asMilliseconds() == 60 * 1000"))
+    MINUTE      (60 * 1000),
+
+    @Invars(@Expression("HOUR.asMilliseconds() == 60 * 60 * 1000"))
+    HOUR        (60 * 60 * 1000),
+
+    /**
+     * The duration of one day. Note that calculations with this duration
+     * are off if the actual day you are talking about contains a leap second.
+     */
+    @Invars(@Expression("DAY.asMilliseconds() == 24 * 60 * 60 * 1000"))
+    DAY         (24 * 60 * 60 * 1000),
+
+    /**
+     * The duration of 7 days. Note that calculations with this duration
+     * are off if the actual week you are talking about contains a day with a
+     * leap second.
+     */
+    @Invars(@Expression("WEEK.asMilliseconds() == 7 * 24 * 60 * 60 * 1000"))
+    WEEK        (7 * 24 * 60 * 60 * 1000),
+
+    /**
+     * The duration of 30 days. Note that calculations with this duration
+     * are off if the actual month you are talking about contains a day with a
+     * leap second, and clearly do not take into account the difference between
+     * months with 28, 29, 30 or 31 days.
+     */
+    @Invars(@Expression("MONTH.asMilliseconds() == 30 * 24 * 60 * 60 * 1000"))
+    MONTH       (30 * 24 * 60 * 60 * 1000),
+
+    /**
+     * The duration of 90 days. Note that calculations with this duration
+     * are off if the actual quarter you are talking about contains a day with a
+     * leap second , and clearly do not take into account the difference between
+     * months with 28, 29, 30 or 31 days.
+     */
+    @Invars(@Expression("QUARTER.asMilliseconds() == 90 * 24 * 60 * 60 * 1000"))
+    QUARTER     (3 * 30 * 24 * 60 * 60 * 1000),
+
+    /**
+     * The duration of 365 days. Note that calculations with this duration
+     * are off if the actual year contains a leap day or a day with a leap second.
+     */
+    @Invars(@Expression("YEAR.asMilliseconds() == 365 * 24 * 60 * 60 * 1000"))
+    YEAR        (365 * 24 * 60 * 60 * 1000),
+
+    /**
+     * 10 years. In every decennium, there are 2 leap years. Note that calculations
+     * with this duration does not take into account leap seconds, nor non-leap
+     * years on century breaks (or exceptions on that on milbreaks).
+     */
+    @Invars(@Expression("DECENNIUM.asMilliseconds() == ((10 * 365) + 2) * 24 * 60 * 60 * 1000"))
+    DECENNIUM   (((10 * 365) + 2) * 24 * 60 * 60 * 1000),
+
+    /**
+     * 100 years. In every century, there are 24 leap years (100 / 4, minus the
+     * non-leap year on the century break). Note that calculations with this duration
+     * does not take into account leap seconds, nor the exception that
+     * a century break that is also a millennium break indeed is a leap year.
+     */
+    @Invars(@Expression("CENTURY.asMilliseconds() == ((100 * 365) + 24) * 24 * 60 * 60 * 1000"))
+    CENTURY     (((100 * 365) + 24) * 24 * 60 * 60 * 1000),
+
+    /**
+     * 1000 years. In every century, there are 241 leap years (the leap years in a century (24)
+     * plus the leap year of the millennium break). Note that calculations with this duration
+     * does not take into account leap seconds.
+     */
+    @Invars(@Expression("MILLENNNIUM.asMilliseconds() == ((1000 * 365) + 241) * 24 * 60 * 60 * 1000"))
+    MILLENNIUM   (((1000 * 365) + 241) * 24 * 60 * 60 * 1000);
+
+    private Unit(int milliseconds) {
       $milliseconds = milliseconds;
     }
 
+    /**
+     * The factor <code>{@link #MILLISECOND} / this</code>}.
+     */
+    @Basic
     public int asMilliseconds() {
       return $milliseconds;
     }
@@ -83,24 +141,33 @@ public class Duration extends AbstractImmutableValue {
 
   }
 
+  @MethodContract(
+    pre  = {
+      @Expression("d >= 0"),
+      @Expression("d != 0 ? unit != null")
+    },
+    post = @Expression("as(MILLISECOND) == d * unit.asMilliseconds()")
+  )
   public Duration(long d, Unit unit) {
+    pre(d >= 0, "d >= 0");
+    pre(d != 0 ? unit != null : true, "unit != null");
     $millis = (d == 0) ? 0 : d * unit.asMilliseconds();
   }
 
+  @Basic(pre = @Expression("unit != null"),
+         invars = @Expression("for (Unit u : Unit.values()) {as(u) >= 0}"))
   public final float as(Unit unit) {
+    preArgumentNotNull(unit, "unit");
     return $millis / unit.asMilliseconds();
   }
 
-//  public final int asFloor(Unit unit) {
-//    return (int)floor($millis / unit.asMilliseconds());
-//  }
-
+  @Invars(@Expression("$millis >= 0"))
   private final long $millis;
 
   @Override
+  @MethodContract(post = @Expression("as(MILLESECOND) == ((Duration)other).as(MILLISECOND)"))
   public boolean equals(Object other) {
-    return other != null && other instanceof Duration &&
-           $millis == ((Duration)other).as(MILLISECOND);
+    return super.equals(other) && $millis == ((Duration)other).as(MILLISECOND);
   }
 
   @Override
@@ -112,6 +179,27 @@ public class Duration extends AbstractImmutableValue {
   public String toString() {
     NumberFormat nf = NumberFormat.getIntegerInstance();
     return nf.format($millis) + " ms";
+  }
+
+  @MethodContract(post = {
+    @Expression("o == null || as(MILLESECOND) < o.as(MILLISECOND) ? -1 : (as(MILLISECOND) == o.as(MILLISECOND) ? 0 : +1)")
+  })
+  public int compareTo(Duration o) {
+    if (o == null) {
+      return -1;
+    }
+    else {
+      long other = o.$millis;
+      if ($millis < other) {
+        return -1;
+      }
+      else if ($millis == other) {
+        return 0;
+      }
+      else {
+        return 1;
+      }
+    }
   }
 
 }
