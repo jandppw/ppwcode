83c048f723b0943c1ccb00b0b3b3062cab0957c6
ppw.bot <ppw.bot@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-10-27 23:52:36 +0000

    svn_spider [r1824]
    working root:
      https://ppwcode.googlecode.com/svn/java/value/trunk
    
    fixes:
      src/main/java/org/ppwcode/value_III/time/interval/jpa/openjpa/DayDateTimeIntervalValueHandler.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/value_III/time/interval/jpa/openjpa/DeterminateIntradayTimeIntervalValueHandler.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/value_III/time/interval/jpa/openjpa/IntradayTimeIntervalValueHandler.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/main/java/org/ppwcode/value_III/time/interval/jpa/openjpa/package.html
        +svn:eol-style:       native
        +svn:keywords:        Date Rev URL
        +svn:mime-type:       text/html
      src/test/java/org/ppwcode/value_III/time/interval/AbstractBeginEndTimeIntervalTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/value_III/time/interval/BeginEndTimeIntervalTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/value_III/time/interval/DayDateTimeIntervalTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/value_III/time/interval/DeterminateIntradayTimeIntervalTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
      src/test/java/org/ppwcode/value_III/time/interval/IntradayTimeIntervalTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source

--- java/value/trunk/src/main/java/org/ppwcode/value_III/time/interval/jpa/openjpa/DeterminateIntradayTimeIntervalValueHandler.java
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/time/interval/jpa/openjpa/DeterminateIntradayTimeIntervalValueHandler.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2004 - $Date: 2008-10-27 20:52:26 +0100 (Mon, 27 Oct 2008) $ by PeopleWare n.v..
+Copyright 2004 - $Date$ by PeopleWare n.v..
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -48,10 +48,10 @@ import org.ppwcode.value_III.time.interval.IntradayTimeInterval;
  * @author Jan Dockx
  * @author Peopleware n.v.
  */
-@Copyright("2008 - $Date: 2008-10-27 20:52:26 +0100 (Mon, 27 Oct 2008) $, PeopleWare n.v.")
+@Copyright("2008 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
-@SvnInfo(revision = "$Revision: 3326 $",
-         date     = "$Date: 2008-10-27 20:52:26 +0100 (Mon, 27 Oct 2008) $")
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
 public final class DeterminateIntradayTimeIntervalValueHandler implements ValueHandler {
 
   public final Column[] map(ValueMapping vm, String name, ColumnIO io, boolean adapt) {
