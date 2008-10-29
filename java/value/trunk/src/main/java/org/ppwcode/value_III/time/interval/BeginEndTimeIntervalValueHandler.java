71301870d6925bc3170648b870dba3ac9b33cf1a
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-10-29 22:07:55 +0000

    moved value handlers next to interval classes

--- java/value/trunk/src/main/java/org/ppwcode/value_III/time/interval/BeginEndTimeIntervalValueHandler.java
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/time/interval/BeginEndTimeIntervalValueHandler.java
@@ -14,11 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 </license>*/
 
-package org.ppwcode.value_III.time.interval.jpa.openjpa;
+package org.ppwcode.value_III.time.interval;
 
 
 import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
-import static org.ppwcode.value_III.time.DateHelpers.isDayDate;
 import static org.ppwcode.vernacular.exception_II.ProgrammingErrorHelpers.unexpectedException;
 
 import java.sql.SQLException;
@@ -36,14 +35,11 @@ import org.apache.openjpa.meta.JavaTypes;
 import org.ppwcode.metainfo_I.Copyright;
 import org.ppwcode.metainfo_I.License;
 import org.ppwcode.metainfo_I.vcs.SvnInfo;
-import org.ppwcode.value_III.time.interval.DeterminateIntradayTimeInterval;
-import org.ppwcode.value_III.time.interval.IllegalIntervalException;
-import org.ppwcode.value_III.time.interval.IntradayTimeInterval;
 
 
 /**
- * A OpenJPA value handler for {@link DeterminateIntradayTimeInterval}. The day date is stored
- * seperately in a column, and 2 other columns hold the begin and end time.
+ * A OpenJPA value handler for {@link BeginEndTimeInterval}. Begin and end are stored in 2
+ * columns in the database as {@code TIMESTAMP}.
  *
  * @author Jan Dockx
  * @author Peopleware n.v.
@@ -52,24 +48,16 @@ import org.ppwcode.value_III.time.interval.IntradayTimeInterval;
 @License(APACHE_V2)
 @SvnInfo(revision = "$Revision$",
          date     = "$Date$")
-public final class DeterminateIntradayTimeIntervalValueHandler implements ValueHandler {
+public final class BeginEndTimeIntervalValueHandler implements ValueHandler {
 
   public final Column[] map(ValueMapping vm, String name, ColumnIO io, boolean adapt) {
-    return new Column[] {dayColumn(name), timeColumn(name + "_begin"), timeColumn(name + "_end")};
+    return new Column[] {timestampColumn(name + "_begin"), timestampColumn(name + "_end")};
   }
 
-  private Column dayColumn(String name) {
-    Column c = new Column();
-    c.setName(name + "_day");
-    c.setType(Types.DATE);
-    c.setJavaType(JavaTypes.DATE);
-    return c;
-  }
-
-  private Column timeColumn(String name) {
+  private Column timestampColumn(String name) {
     Column c = new Column();
     c.setName(name);
-    c.setType(Types.TIME);
+    c.setType(Types.TIMESTAMP);
     c.setJavaType(JavaTypes.DATE);
     return c;
   }
@@ -89,15 +77,12 @@ public final class DeterminateIntradayTimeIntervalValueHandler implements ValueH
 
   public Object toDataStoreValue(ValueMapping vm, Object val, JDBCStore store) {
     try {
-      DeterminateIntradayTimeInterval beTi = (DeterminateIntradayTimeInterval)val;
-      Date day = beTi.getDay();
-      Date beginTime = beTi.getBegin();
-      Date endTime = beTi.getEnd();
-      return new Date[] {day, beginTime, endTime};
+      BeginEndTimeInterval beTi = (BeginEndTimeInterval)val;
+      return new Date[] {beTi.getBegin(), beTi.getEnd()};
     }
     catch (ClassCastException exc) {
       unexpectedException(exc, "trying to handle " + val + " with " +
-                          DeterminateIntradayTimeIntervalValueHandler.class.getName() + ", but that can't handle that type");
+                          BeginEndTimeIntervalValueHandler.class.getName() + ", but that can't handle that type");
     }
     return null; // make compiler happy
   }
@@ -105,13 +90,9 @@ public final class DeterminateIntradayTimeIntervalValueHandler implements ValueH
   public Object toObjectValue(ValueMapping vm, Object fromDb) {
     try {
       Object[] dates = (Date[])fromDb;
-      Date day = (Date)dates[0];
-      assert day == null || isDayDate(day);
-      Date beginTime = (Date)dates[1];
-      beginTime = addTime(day, beginTime);
-      Date endTime = (Date)dates[2];
-      endTime = addTime(day, endTime);
-      return new DeterminateIntradayTimeInterval(beginTime, endTime);
+      Date begin = (Date)dates[0];
+      Date end = (Date)dates[1];
+      return new BeginEndTimeInterval(begin, end);
     }
     catch (NullPointerException exc) {
       unexpectedException(exc, "data received from database is not as expected: we can't deal with null");
@@ -120,22 +101,12 @@ public final class DeterminateIntradayTimeIntervalValueHandler implements ValueH
       unexpectedException(exc, "data received from database is not as expected: expected array of 2 values");
     }
     catch (ClassCastException exc) {
-      unexpectedException(exc, "data received from database is not as expected: expected an array of 3 dates");
+      unexpectedException(exc, "data received from database is not as expected: expected an array of 2 dates");
     }
     catch (IllegalIntervalException exc) {
-      unexpectedException(exc, "data received from database did violate invariants for " + IntradayTimeInterval.class);
+      unexpectedException(exc, "data received from database did violate invariants for " + BeginEndTimeInterval.class);
     }
     return null; // make compiler happy
-  }
-
-  private Date addTime(Date day, Date time) {
-    if (day == null || time == null) {
-      return null;
-    }
-    long millies = day.getTime();
-    millies += time.getTime();
-    Date result = new Date(millies);
-    return result;
   }
 
   /**
