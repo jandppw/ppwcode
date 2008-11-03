9f0c522b17cbf36ba7661ac601706cf539097ce3
ppw.bot <ppw.bot@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-11-03 23:37:57 +0000

    svn_spider [r1824]
    working root:
      https://ppwcode.googlecode.com/svn/java/research/jpa/trunk
    
    fixes:
      src/test/java/org/ppwcode/research/jpa/openjpa/valuehandlers/TimeZoneTest.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source

--- java/research/jpa/trunk/src/test/java/org/ppwcode/research/jpa/openjpa/valuehandlers/TimeZoneTest.java
+++ java/research/jpa/trunk/src/test/java/org/ppwcode/research/jpa/openjpa/valuehandlers/TimeZoneTest.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2008 - $Date: 2008-10-29 23:00:59 +0100 (Wed, 29 Oct 2008) $ by PeopleWare n.v.
+Copyright 2008 - $Date$ by PeopleWare n.v.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
