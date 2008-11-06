35e867c44dc0ab3eac9247cd7d0ac054e270f346
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-11-06 16:22:05 +0000

    renamed package "property editors" to "ext"

--- java/value/trunk/src/main/java/org/ppwcode/value_III/ext/java/util/TimeZoneEditor.java
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/ext/java/util/TimeZoneEditor.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 </license>*/
 
-package org.ppwcode.value_III.propertyeditors.java.util;
+package org.ppwcode.value_III.ext.java.util;
 
 
 import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
