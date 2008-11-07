26a1b20a4f31e4c66c955b83d938144b8fa18596
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-11-07 17:03:50 +0000

    start of RRnr (Belgian state person indentification number)

---
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/person/id11n/state/be/BelgianPersonIdentificationNumber.java
@@ -0,0 +1,70 @@
+/*<license>
+Copyright 2004 - $Date: 2008-11-07 16:59:27 +0100 (Fri, 07 Nov 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.ppwcode.value_III.person.id11n.state.be;
+
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+import org.ppwcode.value_III.id11n.AbstractRegexConstrainedIdentifier;
+import org.ppwcode.value_III.id11n.IdentifierIssuingAuthority;
+import org.ppwcode.value_III.id11n.IdentifierSchemeDescription;
+import org.ppwcode.value_III.id11n.IdentifierWellformednessException;
+import org.ppwcode.value_III.person.id11n.PersonIdentifier;
+import org.toryt.annotations_I.Expression;
+import org.toryt.annotations_I.MethodContract;
+import org.toryt.annotations_I.Throw;
+
+
+/**
+ * <p>The identifier issued by the Belgian state for all civilians.</p>
+ *
+ * @author    Jan Dockx
+ * @author    PeopleWare NV
+ */
+@Copyright("2008 - $Date: 2008-11-07 16:59:27 +0100 (Fri, 07 Nov 2008) $, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision: 3471 $",
+         date     = "$Date: 2008-11-07 16:59:27 +0100 (Fri, 07 Nov 2008) $")
+@IdentifierIssuingAuthority(name = "Federal Public Service of the Interior", uri = "http://www.ibz.fgov.be/")
+@IdentifierSchemeDescription("http://www.ibz.rrn.fgov.be/fileadmin/user_upload/Registre/nl/instructies/onderrichtingen_080601.pdf")
+// starting from page 31
+public final class BelgianPersonIdentificationNumber extends AbstractRegexConstrainedIdentifier implements PersonIdentifier {
+
+
+  public final static String REGEX_PATTERN = "MUDO";
+
+  @MethodContract(
+    pre  = {
+      @Expression("_identifier != null"),
+      @Expression("_identifier != EMPTY")
+    },
+    post = {
+      @Expression("identifier == _identifier")
+    },
+    exc  = {
+      @Throw(type = IdentifierWellformednessException.class,
+             cond = @Expression("! regexPattern.matcher(identifier).matches()"))
+    }
+  )
+  public BelgianPersonIdentificationNumber(String identifier) throws IdentifierWellformednessException {
+    super(identifier);
+  }
+
+}
