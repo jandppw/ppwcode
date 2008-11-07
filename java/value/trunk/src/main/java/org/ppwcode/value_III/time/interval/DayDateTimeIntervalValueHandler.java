fa51cb003a86ec1d53f1f628c46526621fd9b5a3
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-11-07 00:20:32 +0000

    immense juggling with time zones and stuff to get this to work; the core is that JDBC default, and thus JPA, uses the default time zone to store to DB and read from DB, so we need to express everything we want in the default time zone, and back

--- java/value/trunk/src/main/java/org/ppwcode/value_III/time/interval/DayDateTimeIntervalValueHandler.java
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/time/interval/DayDateTimeIntervalValueHandler.java
@@ -19,6 +19,8 @@ package org.ppwcode.value_III.time.interval;
 
 import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
 import static org.ppwcode.value_III.time.TimeHelpers.isDayDate;
+import static org.ppwcode.value_III.time.TimeHelpers.move;
+import static org.ppwcode.value_III.time.TimeHelpers.sqlDayDate;
 import static org.ppwcode.vernacular.exception_II.ProgrammingErrorHelpers.unexpectedException;
 
 import java.sql.Types;
@@ -26,11 +28,11 @@ import java.util.Date;
 import java.util.TimeZone;
 
 import org.apache.openjpa.jdbc.kernel.JDBCStore;
+import org.apache.openjpa.jdbc.meta.JavaSQLTypes;
 import org.apache.openjpa.jdbc.meta.ValueMapping;
 import org.apache.openjpa.jdbc.meta.strats.AbstractValueHandler;
 import org.apache.openjpa.jdbc.schema.Column;
 import org.apache.openjpa.jdbc.schema.ColumnIO;
-import org.apache.openjpa.meta.JavaTypes;
 import org.ppwcode.metainfo_I.Copyright;
 import org.ppwcode.metainfo_I.License;
 import org.ppwcode.metainfo_I.vcs.SvnInfo;
@@ -56,14 +58,15 @@ public final class DayDateTimeIntervalValueHandler extends AbstractValueHandler 
   private final TimeZoneValueHandler $timeZoneValueHandler = new TimeZoneValueHandler();
 
   public final Column[] map(ValueMapping vm, String name, ColumnIO io, boolean adapt) {
-    return new Column[] {dateColumn(name + "_begin"), dateColumn(name + "_end"), $timeZoneValueHandler.map(vm, name + "_timezone", io, false)[0]};
+    return new Column[] {dateColumn(name + "_begin"), dateColumn(name + "_end"),
+                         $timeZoneValueHandler.map(vm, name + "_timezone", io, false)[0]};
   }
 
   private Column dateColumn(String name) {
     Column c = new Column();
     c.setName(name);
     c.setType(Types.DATE);
-    c.setJavaType(JavaTypes.DATE);
+    c.setJavaType(JavaSQLTypes.SQL_DATE);
     return c;
   }
 
@@ -74,13 +77,32 @@ public final class DayDateTimeIntervalValueHandler extends AbstractValueHandler 
       if (beTi == null) {
         return new Object[] {null, null, null};
       }
-      return new Object[] {beTi.getBegin(), beTi.getEnd(), $timeZoneValueHandler.toDataStoreValue(vm, beTi.getTimeZone(), store)};
+      /*
+       * BIG NOTE
+       * The day date effectively stored when we try to store d, is the date it is at
+       * the default time zone at time d.
+       * Thus, if d is midnight of day x in time zone tz, and tz is more to the east than the default time zone,
+       * the day date stored is x - 1!!.
+       * To fix this, we will correct what we pass in to be midnight of the date we want in the _default time zone_,
+       * although that point in time does not correspond most of the time with the point in time we actually want.
+       */
+      return new Object[] {midnightDefaultTZ(beTi.getBegin(), beTi.getTimeZone()),
+                           midnightDefaultTZ(beTi.getEnd(), beTi.getTimeZone()),
+                           $timeZoneValueHandler.toDataStoreValue(vm, beTi.getTimeZone(), store)};
     }
     catch (ClassCastException exc) {
       unexpectedException(exc, "trying to handle " + val + " with " +
                           DayDateTimeIntervalValueHandler.class.getName() + ", but that can't handle that type");
     }
     return null; // make compiler happy
+  }
+
+  private static java.sql.Date midnightDefaultTZ(Date d, TimeZone tz) {
+    assert isDayDate(d, tz);
+    TimeZone defaultTz = TimeZone.getDefault();
+    Date moved = move(d, tz, defaultTz);
+    assert isDayDate(moved, defaultTz);
+    return sqlDayDate(moved, defaultTz);
   }
 
   @Override
@@ -93,9 +115,14 @@ public final class DayDateTimeIntervalValueHandler extends AbstractValueHandler 
       if (begin == null && end == null) {
         return null;
       }
-      assert isDayDate(begin, tz);
-      assert isDayDate(end, tz);
-      return new DayDateTimeInterval(begin, end, tz);
+      /*
+       * BIG NOTE
+       * What we get here, via OpenJPA, from the database, is a java.sql.Date that represents
+       * midnight of the effective day date in the database (DATE), expressed in the DEFAULT TIMEZONE,
+       * although we put it in in the timezone tz!
+       * So isDayDate(begin, tz) and isDayDate(end, tz) are probably NOT true.
+       */
+      return new DayDateTimeInterval(midnightTZ(begin, tz), midnightTZ(end, tz), tz);
     }
     catch (ArrayIndexOutOfBoundsException exc) {
       unexpectedException(exc, "data received from database is not as expected: expected array of 2 values");
@@ -107,6 +134,14 @@ public final class DayDateTimeIntervalValueHandler extends AbstractValueHandler 
       unexpectedException(exc, "data received from database did violate invariants for " + BeginEndTimeInterval.class);
     }
     return null; // make compiler happy
+  }
+
+  private Date midnightTZ(Date d, TimeZone tz) {
+    TimeZone defaultTz = TimeZone.getDefault();
+    assert isDayDate(d, defaultTz);
+    Date moved = move(d, defaultTz, tz);
+    assert isDayDate(moved, tz);
+    return moved;
   }
 
 }
