ddbc9b11f04eea97ef70d79e636bd471fd9e1603
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-11-11 22:16:05 +0000

    first version of a Belgian enterprise number; not happy with calling instance methods in a constructor!!!!!!

---
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/organization/id11n/OrganizationIdentifier.java
@@ -0,0 +1,42 @@
+/*<license>
+Copyright 2004 - $Date: 2008-11-09 15:31:55 +0100 (Sun, 09 Nov 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.ppwcode.value_III.organization.id11n;
+
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+import org.ppwcode.value_III.id11n.Identifier;
+
+
+/**
+ * <p>An identifier for organizations.</p>
+ *
+ * @author    Jan Dockx
+ * @author    PeopleWare NV
+ */
+@Copyright("2008 - $Date: 2008-11-09 15:31:55 +0100 (Sun, 09 Nov 2008) $, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision: 3484 $",
+         date     = "$Date: 2008-11-09 15:31:55 +0100 (Sun, 09 Nov 2008) $")
+public interface OrganizationIdentifier extends Identifier {
+
+  // NOP
+
+}
