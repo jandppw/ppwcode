913d74540f344491bdef378e314e7dd3defc829a
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-11-16 14:06:25 +0000

    snapshot

---
+++ java/util/serialization/dev/d20081116-1506/src/main/java/org/ppwcode/util/serialization_I/DoNotSerialize.java
@@ -0,0 +1,60 @@
+/*<license>
+Copyright 2008 - $Date$ by PeopleWare n.v.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.ppwcode.util.serialization_I;
+
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+
+
+/**
+ * Annotation for serilization helpers. The property annotated with this annotation
+ * should not be serialized (on deserialization, the initial value after construction
+ * with the default constructor is kept).
+ *
+ * Usage pattern:
+ * <pre>
+ *   &#64;DoNotSerialize
+ *   public final <var>PropertyType</var> get<var>PropertyName</var>() {
+ *     ...
+ *   }
+ * </pre>
+ *
+ * @author Jan Dockx
+ * @author PeopleWare n.v.
+ */
+@Copyright("2008 - $Date$, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface DoNotSerialize {
+
+  // NOP
+
+}
