3479fe7f0538e266952c6208c23211f25a0f6bd6
jandockx <jandockx@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-11-17 12:01:25 +0000

    user type for remaining time interval types; untested

---
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/time/interval/DeterminateIntradayTimeIntervalUserType.java
@@ -0,0 +1,54 @@
+/*<license>
+Copyright 2004 - $Date: 2008-11-07 01:22:20 +0100 (Fri, 07 Nov 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.ppwcode.value_III.time.interval;
+
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+import org.ppwcode.vernacular.value_III.ImmutableValue;
+
+
+/**
+ * A OpenJPA value handler for {@link DeterminateIntradayTimeInterval}.
+ *
+ * @author Jan Dockx
+ * @author PeopleWare n.v.
+ */
+@Copyright("2008 - $Date: 2008-11-07 01:22:20 +0100 (Fri, 07 Nov 2008) $, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision: 3457 $",
+         date     = "$Date: 2008-11-07 01:22:20 +0100 (Fri, 07 Nov 2008) $")
+public final class DeterminateIntradayTimeIntervalUserType extends AbstractIntradayTimeIntervalUserType {
+
+  @Override
+  public Class<? extends ImmutableValue> returnedClass() {
+    return DeterminateIntradayTimeInterval.class;
+  }
+
+  @Override
+  protected AbstractIntradayTimeInterval createFreshIntradayTimeInterval(Date beginTime, Date endTime, TimeZone tz)
+      throws IllegalTimeIntervalException {
+    return new DeterminateIntradayTimeInterval(beginTime, endTime, tz);
+  }
+
+}
