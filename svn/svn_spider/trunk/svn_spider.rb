b2fce200d159c50f28f492dd9ec4df1dbe53cb7b
ruben.vandeginste <ruben.vandeginste@463e38ab-4f4e-0410-907a-65d2b3888964>
2008-11-20 21:45:51 +0000

    added basic error handling: errors is add to a log along with a timestamp

--- svn/svn_spider/trunk/svn_spider.rb
+++ svn/svn_spider/trunk/svn_spider.rb
@@ -69,93 +69,102 @@ end
 # do the actual checking and fixing for each repository
 @@repositories.each_pair do |repo, info|
 
-  # create a client context
-  ctx = Svn::Client::Context.new
+  begin
 
-  # authentication
-  if ((info["auth"] == nil) || (@@credentials[info["auth"]] == nil)) then
-    # maybe svn already has the authentication info
-    ctx.add_simple_provider
-  else
-    # username + password for authentication
-    ctx.add_simple_prompt_provider(0) do |cred, realm, username, may_save|
-      cred.username = @@credentials[info["auth"]]["username"]
-      cred.password = @@credentials[info["auth"]]["password"]
+    # create a client context
+    ctx = Svn::Client::Context.new
+
+    # authentication
+    if ((info["auth"] == nil) || (@@credentials[info["auth"]] == nil)) then
+      # maybe svn already has the authentication info
+      ctx.add_simple_provider
+    else
+      # username + password for authentication
+      ctx.add_simple_prompt_provider(0) do |cred, realm, username, may_save|
+        cred.username = @@credentials[info["auth"]]["username"]
+        cred.password = @@credentials[info["auth"]]["password"]
+      end
     end
-  end
 
-  # log message configuration
-  @@log = @@log_msg.clone
-  @@log << "working root:\n"
-  @@log << "  " << info["url"] << "\n\n"
-  @@log << "fixes:\n"
-  ctx.set_log_msg_func do |items|
-    [true, @@log]
-  end
+    # log message configuration
+    @@log = @@log_msg.clone
+    @@log << "working root:\n"
+    @@log << "  " << info["url"] << "\n\n"
+    @@log << "fixes:\n"
+    ctx.set_log_msg_func do |items|
+      [true, @@log]
+    end
 
-  # check out a project, or update it if a working copy is found
-  wc_path = File.join @@working_directory,  repo
-  if File.exists? wc_path then
-    ctx.update wc_path
-  else
-    ctx.checkout info["url"], wc_path
-  end
+    # check out a project, or update it if a working copy is found
+    wc_path = File.join @@working_directory,  repo
+    if File.exists? wc_path then
+      ctx.update wc_path
+    else
+      ctx.checkout info["url"], wc_path
+    end
 
-  # walk the file tree and update property lists
-  ctx.list( wc_path, "HEAD", nil, true) do |path, dirent, lock, abs_path|
-    # path is path of file relative to wc_path
-    # abs_path is location of the root of path
-    #        relative to the repository root
-    if not dirent.directory? then
-      new_props = {}
-      # check mime type
-      ext = File.extname(path)
-      mime = (ext == nil) ? nil : mime_type_registry[ext[1..-1]]
-      if ((not ext.empty?) && (mime != nil)) then
-        new_props[Svn::Core::PROP_MIME_TYPE] = mime
-      end
-      # check property filters
-      @@property_filters.each_pair do |key, value|
-        if path =~ key then
-          new_props.merge! value
+    # walk the file tree and update property lists
+    ctx.list( wc_path, "HEAD", nil, true) do |path, dirent, lock, abs_path|
+      # path is path of file relative to wc_path
+      # abs_path is location of the root of path
+      #        relative to the repository root
+      if not dirent.directory? then
+        new_props = {}
+        # check mime type
+        ext = File.extname(path)
+        mime = (ext == nil) ? nil : mime_type_registry[ext[1..-1]]
+        if ((not ext.empty?) && (mime != nil)) then
+          new_props[Svn::Core::PROP_MIME_TYPE] = mime
         end
-      end
-      # fetch the current property list
-      wc_file_path = File.join wc_path, path
-      old_props = {}
-      if dirent.have_props? then
-        props = ctx.proplist wc_file_path
-        # should not be empty since we checked that is has properties
-        if not props.empty? then
-          # only 1 entry since we take one file at a time
-          props.each do |pli|
-            old_props.merge! pli.props
+        # check property filters
+        @@property_filters.each_pair do |key, value|
+          if path =~ key then
+            new_props.merge! value
           end
         end
-      end
-      # compare property lists and update new_props
-      new_props.clone.each_key do |k|
-        new_props.delete k if new_props[k] == old_props[k]
-      end
-      # update the property list in the working copy
-      if not new_props.empty? then
-        @@log << "  " << path << "\n"
-      end
-      new_props.each_pair do |key, value|
-        ctx.propset key, value, wc_file_path
-        # update log
-        if (old_props[key] != nil) then
-          @@log << "    -" << key << ":"
-          @@log << (" " * [(20 - key.size), 1].max)
-          @@log << old_props[key] << "\n"
+        # fetch the current property list
+        wc_file_path = File.join wc_path, path
+        old_props = {}
+        if dirent.have_props? then
+          props = ctx.proplist wc_file_path
+          # should not be empty since we checked that is has properties
+          if not props.empty? then
+            # only 1 entry since we take one file at a time
+            props.each do |pli|
+              old_props.merge! pli.props
+            end
+          end
         end
-        @@log << "    +" << key << ":"
-        @@log << (" " * [(20 - key.size), 1].max)
-        @@log << value << "\n"
+        # compare property lists and update new_props
+        new_props.clone.each_key do |k|
+          new_props.delete k if new_props[k] == old_props[k]
+        end
+        # update the property list in the working copy
+        if not new_props.empty? then
+          @@log << "  " << path << "\n"
+        end
+        new_props.each_pair do |key, value|
+          ctx.propset key, value, wc_file_path
+          # update log
+          if (old_props[key] != nil) then
+            @@log << "    -" << key << ":"
+            @@log << (" " * [(20 - key.size), 1].max)
+            @@log << old_props[key] << "\n"
+          end
+          @@log << "    +" << key << ":"
+          @@log << (" " * [(20 - key.size), 1].max)
+          @@log << value << "\n"
+        end
       end
     end
-  end
 
-  # finally commit the changes in the working copy
-  ctx.commit wc_path
+    # finally commit the changes in the working copy
+    ctx.commit wc_path
+
+    # error handling: just log it for now
+  rescue
+    File.open File.join(File.dirname(__FILE__), "spider.log"), "w+" do |f|
+      f.write "#{Time.now.getutc} :: error in repo: #{repo}\n"
+    end
+  end
 end
