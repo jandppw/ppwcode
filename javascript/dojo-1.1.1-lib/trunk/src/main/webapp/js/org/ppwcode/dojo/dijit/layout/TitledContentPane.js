f66b9a184df4bff09d68611025bb5bba9f8a8443
peter.sinjan <peter.sinjan@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-01-21 08:37:56 +0000

    new component titledContentPane

---
+++ javascript/dojo-1.1.1-lib/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/layout/TitledContentPane.js
@@ -0,0 +1,153 @@
+dojo.provide("org.ppwcode.dojo.dijit.layout.TitledContentPane");
+dojo.experimental("org.ppwcode.dojo.dijit.layout.TitledContentPane");
+
+dojo.require("dojox.layout.ContentPane");
+dojo.require("dojox.fx.Shadow");
+dojo.require("dijit._Templated");
+dojo.require("dijit._Widget");
+
+dojo.declare("org.ppwcode.dojo.dijit.layout.TitledContentPane",
+	[ dojox.layout.ContentPane, dijit._Templated ],
+	{
+	// summary:
+	//		A non-modal Floating window.
+	//
+	// description:
+	// 		Makes a dijit.ContentPane float and draggable by it's title [similar to TitlePane]
+	// 		and over-rides onClick to onDblClick for wipeIn/Out of containerNode
+	// 		provides minimize(dock) / show() and hide() methods, and resize [almost] 
+
+	// title: String
+	//		Title to use in the header
+	title: "",
+
+	/*=====
+	// iconSrc: String
+	//		[not implemented yet] will be either icon in titlepane to left
+	//		of Title, and/or icon show when docked in a fisheye-like dock
+	//		or maybe dockIcon would be better?
+	iconSrc: null,
+	=====*/
+
+	'class': "",
+	// contentClass: String
+	// 		The className to give to the inner node which has the content
+	contentClass: "dojoxTitledPaneContent",
+	
+	paddingSize: 10,
+	showShadow: false,
+
+	// privates:
+	_currentState: null,
+	_shadow: null,
+
+	templateString: null,
+	templatePath: dojo.moduleUrl("org", "ppwcode/dojo/dijit/layout/templates/TitledContentPane.html"),
+	
+	postCreate: function(){
+	
+		this.setTitle(this.title);
+		if (this.showShadow) {
+			this._shadow = new dojox.fx.Shadow({node:this.domNode});
+			this.setShadowBorder();
+			this._shadow.startup();
+		}
+
+		this.inherited(arguments);
+	},
+	
+	startup: function(){
+		if(this._started){ return; }
+
+		// wire up the tablist and its tabs
+		this.inherited(arguments);
+
+		if(dojo.isSafari){
+			// sometimes safari 3.0.3 miscalculates the height of the tab labels, see #4058
+			setTimeout(dojo.hitch(this, "layout"), 0);
+		}
+		
+		if(dojo.isIE){
+			this.canvas.style.overflow = "auto";
+		} else {
+			this.containerNode.style.overflow = "auto";
+		}		
+		this.resize(dojo.coords(this.domNode));
+		this._started = true;
+	},
+
+	setTitle: function(/* String */ title){
+		// summary: Update the Title bar with a new string
+		this.titleNode.innerHTML = title; 
+		this.title = title; 
+	},
+	
+	calcSize: function(/*DomNode|String*/dn) {
+		return dojo.contentBox(dn);
+	},
+	
+	setShadowBorder: function() {
+		var dns = this.domNode.style;
+		dns.margin = this._shadow.shadowThickness + "px";
+	},
+		
+	//
+	resize: function(/*Object*/dim){
+		// summary: Size the FloatingPane and place accordingly
+		if (!dim){
+			dim = this.calcSize(this.domNode);
+		}
+	
+		//Adding padding to contentPane
+		var dns = this.canvas.style;
+		dns.margin = this.paddingSize+"px";
+		
+		// Now resize canvas
+		var factor = 2;
+		if(dojo.isIE){
+			factor = 4;
+		}
+		var mbCanvas = { l: 0, t: 0, w: dim.w - (this.paddingSize * factor), h: (dim.h - this.focusNode.offsetHeight) - (this.paddingSize * factor)};
+		
+		
+		// If the single child can resize, forward resize event to it so it can
+		// fit itself properly into the content area
+		this._checkIfSingleChild();
+		if(this._singleChild && this._singleChild.resize){
+			this._singleChild.resize(mbCanvas);
+		}
+
+		if (this._shadow) {
+			this._shadow.resize();
+		}
+	},	
+	
+	layout: function(){
+		// Summary: Configure the content pane to take up all the space except for where the tabs are
+		//if(!this.doLayout){ return; }
+		console.log("----------- Layout set -----------");
+
+		// position and size the titles and the container node
+		var titleAlign = this.tabPosition.replace(/-h/,"");
+		var children = [
+			{ domNode: this.titleNode.domNode, layoutAlign: titleAlign },
+			{ domNode: this.containerNode, layoutAlign: "client" }
+		];
+		dijit.layout.layoutChildren(this.domNode, this._contentBox, children);
+
+		// Compute size to make each of my children.
+		// children[1] is the margin-box size of this.containerNode, set by layoutChildren() call above
+		this._containerContentBox = dijit.layout.marginBox2contentBox(this.containerNode, children[1]);
+
+		if(this.selectedChildWidget){
+			this._showChild(this.selectedChildWidget);
+			if(this.doLayout && this.selectedChildWidget.resize){
+				this.selectedChildWidget.resize(this._containerContentBox);
+			}
+		}
+	},	
+	
+	destroy: function(){
+		this.inherited(arguments);
+	}
+});
