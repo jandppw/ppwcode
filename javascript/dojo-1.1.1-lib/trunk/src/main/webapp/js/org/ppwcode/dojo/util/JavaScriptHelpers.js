50b0cacc3117645d37d3d080338e2a4671352a6d
peter.sinjan <peter.sinjan@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-02-09 11:04:18 +0000

    dwr 3 changes

--- javascript/dojo-1.1.1-lib/trunk/src/main/webapp/js/org/ppwcode/dojo/util/JavaScriptHelpers.js
+++ javascript/dojo-1.1.1-lib/trunk/src/main/webapp/js/org/ppwcode/dojo/util/JavaScriptHelpers.js
@@ -1,7 +1,12 @@
 dojo.provide("org.ppwcode.dojo.util.JavaScriptHelpers");
 
 org.ppwcode.dojo.util.JavaScriptHelpers.getFunctionName = function(/*Function*/func) {
-	var s = func.toString();
-	s = s.substr(0, s.indexOf("()")).substr("function ".length);
-	return s;
+	var result = null;
+	if (func.$dwrClassName) {
+		result = func.$dwrClassName;
+	} else {
+		var s = func.toString();
+		result = s.substr(0, s.indexOf("()")).substr("function ".length);
+	}
+	return result;
 }
