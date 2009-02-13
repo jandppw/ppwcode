e4f6063818918342da13f26d4df0dbd5693001a5
Tom.Mahieu <Tom.Mahieu@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-02-13 08:22:11 +0000

    added tests for PpwCrudForm (still lacking error handling tests)

--- javascript/dojo-1.1.1-lib/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/tests/form/module.js
+++ javascript/dojo-1.1.1-lib/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/tests/form/module.js
@@ -1,6 +1,7 @@
 dojo.provide("org.ppwcode.dojo.dijit.tests.form.module");
 
 if(dojo.isBrowser){
-	doh.registerUrl("ppwcode.form.testtest", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/testtest.html"));
+//	doh.registerUrl("ppwcode.form.testtest", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/testtest.html"));
 	doh.registerUrl("ppwcode.form.PpwCrudForm", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/PpwCrudForm.html"));
+	doh.registerUrl("ppwcode.form.ObjectGraphPpwCrudForm", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/ObjectGraphPpwCrudForm.html"));
 }
