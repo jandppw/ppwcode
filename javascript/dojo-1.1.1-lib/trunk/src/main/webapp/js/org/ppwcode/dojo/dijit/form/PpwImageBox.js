d7757cde0f789222b31fe546b1aec466d1c2ce72
Tom.Mahieu <Tom.Mahieu@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-02-27 08:32:13 +0000

    FormWidget that takes a URL of an image as value and displays that image.
    Needs improvemnt on how the widget behaves at initialization time

---
+++ javascript/dojo-1.1.1-lib/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/form/PpwImageBox.js
@@ -0,0 +1,21 @@
+dojo.provide("org.ppwcode.dojo.dijit.form.PpwImageBox");
+
+dojo.require("dijit._Templated");
+dojo.require("dijit.form._FormWidget");
+
+dojo.declare(
+	"org.ppwcode.dojo.dijit.form.PpwImageBox",
+	dijit.form._FormValueWidget,
+	{
+		imgSrc: "",
+		
+		templateString: "<img dojoAttachPoint='focusNode' src='${imgSrc}'></img>",
+		
+		setValue: function(newValue) {
+			console.log('PpwImageBox::setValue : ' + newValue);
+			dojo.attr(this.domNode, "src", newValue);
+			this.inherited(arguments);
+		}
+	}
+);
+		     
