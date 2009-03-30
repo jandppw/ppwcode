b7e05117e0e99e8418af43fafd4d1498386f1ba6
adriaan.peeters <adriaan.peeters@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-03-30 08:32:39 +0000

    Removed unused imports

--- demo/FacebookDemo/trunk/src/main/java/be/peopleware/facebookdemo/persistence/jpa/JpaMovieDao.java
+++ demo/FacebookDemo/trunk/src/main/java/be/peopleware/facebookdemo/persistence/jpa/JpaMovieDao.java
@@ -1,13 +1,11 @@
 package be.peopleware.facebookdemo.persistence.jpa;
 
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
 import org.ppwcode.vernacular.persistence_III.dao.jpa.AbstractJpaDao;
 
