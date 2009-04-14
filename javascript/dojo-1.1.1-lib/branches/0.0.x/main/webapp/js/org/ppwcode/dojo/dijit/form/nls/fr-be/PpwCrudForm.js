3987b938599dfab7e9d132523ff769e922cadcf0
Tom.Mahieu <Tom.Mahieu@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-04-14 12:43:17 +0000

    for a change... tagged wrong

--- javascript/dojo-1.1.1-lib/branches/0.0.x/main/webapp/js/org/ppwcode/dojo/dijit/form/nls/fr-be/PpwCrudForm.js
+++
@@ -1,8 +0,0 @@
-({"editButtonLabel":"Editer",
-  "saveButtonLabel":"Sauver",
-  "updateButtonLabel":"Actualiser",
-  "deleteButtonLabel":"Supprimer",
-  "cancelButtonLabel":"Annuler",
-  "errorBoxTitle":"Fautes suppl&eacute;mentaires",
-  "savingMessage": "En train de sauver...",
-  "updatingMessage": "En train d'actualiser..."})
