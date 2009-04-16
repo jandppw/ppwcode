2d9b949c809bcc55f4d2c6da5c0bb6668a90e1c3
tom.mahieu <tom.mahieu@gmail.com>
2009-04-16 07:26:43 +0000

    Bloody subversion client differences

--- javascript/dojo-lib/trunk/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/form/nls/PpwCrudForm.js
+++
@@ -1,8 +0,0 @@
-({"editButtonLabel":"Edit",
-  "saveButtonLabel":"Save",
-  "updateButtonLabel":"Update",
-  "deleteButtonLabel":"Delete",
-  "cancelButtonLabel":"Cancel",
-  "errorBoxTitle":"Additional Errors",
-  "savingMessage": "Saving...",
-  "updatingMessage": "Updating..."})
--- javascript/dojo-lib/trunk/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/period/nls/PeriodForm.js
+++
@@ -1,6 +0,0 @@
-({
-	"dateLabel":"Datum",
-	"startTimeLabel":"Start Tijd",
-	"endTimeLabel":"Eind Tijd",
-	"timeZone": "Europe/Brussels"
-})
