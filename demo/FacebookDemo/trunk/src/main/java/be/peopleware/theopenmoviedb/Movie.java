7a69cca8890f33c4a3b06811a4ee8db61f876a9c
adriaan.peeters <adriaan.peeters@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-04-20 14:34:16 +0000

    Intial support for movie poster image

---
+++ demo/FacebookDemo/trunk/src/main/java/be/peopleware/theopenmoviedb/Movie.java
@@ -0,0 +1,51 @@
+package be.peopleware.theopenmoviedb;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Movie {
+	
+	private double score;
+	private int popularity;
+	private String title;
+	
+	private String imdbId;
+	private String shortOverview;
+	
+	private Map<String, String> posters = new HashMap<String, String>();
+	
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public int getPopularity() {
+		return popularity;
+	}
+	public void setPopularity(int popularity) {
+		this.popularity = popularity;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getImdbId() {
+		return imdbId;
+	}
+	public void setImdbId(String imdbId) {
+		this.imdbId = imdbId;
+	}
+	public String getShortOverview() {
+		return shortOverview;
+	}
+	public void setShortOverview(String shortOverview) {
+		this.shortOverview = shortOverview;
+	}
+	public Map<String, String> getPosters() {
+		return posters;
+	}
+
+}
