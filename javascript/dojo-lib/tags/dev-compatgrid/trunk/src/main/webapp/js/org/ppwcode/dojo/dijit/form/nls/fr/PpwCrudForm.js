18a97bc49fa280f5d2a429f4d679691d281a5521
tom.mahieu <tom.mahieu@gmail.com>
2009-04-21 05:46:04 +0000

    tagging version before moving to dojox.grid.DataGrid

---
+++ javascript/dojo-lib/tags/dev-compatgrid/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/form/nls/fr/PpwCrudForm.js
@@ -0,0 +1,8 @@
+({"editButtonLabel":"Editer",
+  "saveButtonLabel":"Sauver",
+  "updateButtonLabel":"Actualiser",
+  "deleteButtonLabel":"Supprimer",
+  "cancelButtonLabel":"Annuler",
+  "errorBoxTitle":"Fautes suppl&eacute;mentaires",
+  "savingMessage": "En train de sauver...",
+  "updatingMessage": "En train d'actualiser..."})
