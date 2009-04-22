482ac4a63341a142fb59873e1f6e54f7f85f8588
Tom.Mahieu <Tom.Mahieu@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-04-22 08:33:55 +0000

    use outputstreamwriter with utf-8 charset instead

--- demo/FacebookDemo/trunk/src/main/java/be/peopleware/theopenmoviedb/servlet/MovieServlet.java
+++ demo/FacebookDemo/trunk/src/main/java/be/peopleware/theopenmoviedb/servlet/MovieServlet.java
@@ -1,6 +1,8 @@
 package be.peopleware.theopenmoviedb.servlet;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -17,52 +19,54 @@ public class MovieServlet extends HttpServlet {
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("application/json; charset=UTF-8");
 		ServletOutputStream out = response.getOutputStream();
+		OutputStreamWriter writer = new OutputStreamWriter(out, Charset.forName("UTF-8"));
 		
 		String queryId = request.getParameter("id");
 		String queryTitle = request.getParameter("title");
 		
 		if (queryId != null && queryId.length() > 0) {
-			handleIdRequest(out, queryId);
+			handleIdRequest(writer, queryId);
 		} else if (queryTitle != null && queryTitle.length() >= 3) {
-			handleTitleRequest(out, queryTitle);
+			handleTitleRequest(writer, queryTitle);
 		} else {
-			handleEmptyRequest(out);
+			handleEmptyRequest(writer);
 		}
 		
-		out.close();
+		writer.close();
 		
 	}
 	
-	private void handleIdRequest(ServletOutputStream out, String queryId) throws IOException {
+	private void handleIdRequest(OutputStreamWriter writer, String queryId) throws IOException {
+
 		Movie movie = Util.searchForMovie(queryId);
 		if (movie != null) {
-			out.println("{ identifier: 'movieId', items:[");
-			out.print("{title: \"" + movie.getTitle() + "\", movieId: \"" + movie.getId() + "\" }");
-			out.println("]}");
+			writer.write("{ identifier: 'movieId', items:[");
+			writer.write("{title: \"" + movie.getTitle() + "\", movieId: \"" + movie.getId() + "\" }");
+			writer.write("]}");
 		} else {
-			handleEmptyRequest(out);
+			handleEmptyRequest(writer);
 		}
 		
 	}
 	
-	private void handleTitleRequest(ServletOutputStream out, String queryTitle) throws IOException {
+	private void handleTitleRequest(OutputStreamWriter writer, String queryTitle) throws IOException {
 		
 		List<Movie> movies = Util.searchForMovies(queryTitle);
 		
-		out.println("{ identifier: 'movieId', items:[");
+		writer.write("{ identifier: 'movieId', items:[");
 	
 		for (int i = 0; i < movies.size(); i++) {
-			out.print("{title: \"" + movies.get(i).getTitle() + "\", movieId: \"" + movies.get(i).getId() + "\" }");
+			writer.write("{title: \"" + movies.get(i).getTitle() + "\", movieId: \"" + movies.get(i).getId() + "\" }");
 			if (i < movies.size() -1)
-				out.println(",");
+				writer.write(",");
 		}
 	
-		out.println("]}");
+		writer.write("]}");
 		
 	}
 	
-	private void handleEmptyRequest(ServletOutputStream out) throws IOException {
-		out.println("{ identifier: 'movieId', items:[ ]}");
+	private void handleEmptyRequest(OutputStreamWriter writer) throws IOException {
+		writer.write("{ identifier: 'movieId', items:[ ]}");
 	}
 
 }
