32ba43121ba1657a4a6fe8832b2d2e0079aba706
Tom.Mahieu <Tom.Mahieu@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-04-23 12:05:55 +0000

    removing annoying prefixes from PpwCrudForm and ViewFormCrudScenariosDwr removing unnecessary nl-be and fr-be translations.

--- javascript/dojo-lib/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/tests/form/module.js
+++ javascript/dojo-lib/trunk/src/main/webapp/js/org/ppwcode/dojo/dijit/tests/form/module.js
@@ -2,12 +2,12 @@ dojo.provide("org.ppwcode.dojo.dijit.tests.form.module");
 
 if(dojo.isBrowser){
 //	doh.registerUrl("ppwcode.form.testtest", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/testtest.html"));
-	doh.registerUrl("ppwcode.form.PpwCrudForm", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/PpwCrudForm.html"));
+	doh.registerUrl("ppwcode.form.CrudForm", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/CrudForm.html"));
 	doh.registerUrl("ppwcode.form.PpwMasterView", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/PpwMasterView.html"));
 	doh.registerUrl("ppwcode.form.MasterView", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/MasterView.html"));
 	doh.registerUrl("ppwcode.form.MasterViewWithoutIdPropName", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/MasterViewWithoutIdPropName.html"));
 	doh.registerUrl("ppwcode.form.PpwEditableDataViewBox", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/PpwEditableDataViewBox.html"));
 	doh.registerUrl("ppwcode.form.EditableDataViewBox", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/EditableDataViewBox.html"));
 	doh.registerUrl("ppwcode.form.SelectDataViewBox", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/SelectDataViewBox.html"));
-	doh.registerUrl("ppwcode.form.ObjectGraphPpwCrudForm", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/ObjectGraphPpwCrudForm.html"));
+	doh.registerUrl("ppwcode.form.ObjectGraphCrudForm", dojo.moduleUrl("org", "ppwcode/dojo/dijit/tests/form/ObjectGraphCrudForm.html"));
 }
