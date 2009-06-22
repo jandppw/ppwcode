929a45867c744fae107f309bd4f6bf7b5e1870fa
dvankeer <dvankeer@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-06-22 14:00:46 +0000

    This is a generic UserType and should not user a specific type as OrganizationIdentifier.

--- java/value/trunk/src/main/java/org/ppwcode/value_III/id11n/IdentifierCompositeUserType.java
+++ java/value/trunk/src/main/java/org/ppwcode/value_III/id11n/IdentifierCompositeUserType.java
@@ -16,12 +16,13 @@ limitations under the License.
 
 package org.ppwcode.value_III.id11n;
 
-import java.lang.reflect.InvocationTargetException;
 import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.SessionImplementor;
@@ -31,17 +32,21 @@ import org.ppwcode.metainfo_I.License;
 import org.ppwcode.metainfo_I.vcs.SvnInfo;
 import org.ppwcode.util.reflect_I.InstanceHelpers;
 import org.ppwcode.util.reflect_I.TypeHelpers;
-import org.ppwcode.value_III.organization.id11n.OrganizationIdentifier;
 import org.ppwcode.vernacular.value_III.hibernate3.AbstractImmutableValueCompositeUserType;
 
 /**
- * <p>Hibernate 3 user type to store and retrieve {@link Identifier} instances.</p>
- * <p>{@link BeEnterpriseNumber} instances are stored in 2 columns of type VARCHAR.
- *   The first contains the FQCN of the actual identifier class to which the instance belongs.
- *   The second column contains the identifier. Both are limited to 255 characters.</p>
- *
- * @author    Ruben Vandeginste
- * @author    PeopleWare NV
+ * <p>
+ * Hibernate 3 user type to store and retrieve {@link Identifier} instances.
+ * </p>
+ * <p>
+ * {@link Identifier} instances are stored in 2 columns of type VARCHAR. The first contains the FQCN of the actual
+ * identifier class to which the instance belongs. The second column contains the identifier. Both are limited to 255
+ * characters.
+ * </p>
+ * 
+ * @author Ruben Vandeginste
+ * @author David Van Keer
+ * @author PeopleWare NV
  */
 @Copyright("2008 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
@@ -50,8 +55,8 @@ import org.ppwcode.vernacular.value_III.hibernate3.AbstractImmutableValueComposi
 public final class IdentifierCompositeUserType extends AbstractImmutableValueCompositeUserType {
 
   /**
-   * Which type we return is defined by a parameter set on the definition of this user type.
-   * This should be a subtype of {@link Identifier}.
+   * Which type we return is defined by a parameter set on the definition of this user type. This should be a subtype of
+   * {@link Identifier}.
    */
   @Override
   public Class<? extends Identifier> returnedClass() {
@@ -59,6 +64,7 @@ public final class IdentifierCompositeUserType extends AbstractImmutableValueCom
   }
 
   public Type[] getPropertyTypes() {
+    // TODO (dvankeer): Why do we use the String Type instead of the ClassType to identify the class property?
     return new Type[] { Hibernate.STRING, Hibernate.STRING };
   }
 
@@ -68,47 +74,42 @@ public final class IdentifierCompositeUserType extends AbstractImmutableValueCom
 
   public Object getPropertyValue(Object obj, int prop) {
     try {
-      OrganizationIdentifier id = (OrganizationIdentifier) obj;
+      Identifier id = (Identifier) obj;
       if (prop == 0) {
         return id.getClass().getCanonicalName();
       } else if (prop == 1) {
         return id.getIdentifier();
       } else {
-        throw new HibernateException("getPropertyValue with wrong index for "+returnedClass());
+        throw new HibernateException("getPropertyValue with wrong index for " + returnedClass());
       }
     } catch (ClassCastException exc) {
-      throw new HibernateException("cannot cast component into "+returnedClass(), exc);
+      throw new HibernateException("cannot cast component into " + returnedClass(), exc);
     }
   }
 
   public void nullSafeSet(PreparedStatement st, Object value, int index, SessionImplementor session)
-  throws HibernateException, SQLException {
+      throws HibernateException, SQLException {
     if (value != null && !returnedClass().isInstance(value)) {
       throw new HibernateException("this user type can only handle values of type "
-                                   + returnedClass().getCanonicalName()
-                                   + "; "
-                                   + value.getClass().getCanonicalName()
-                                   + " is not supported");
-    }
-    else if (value == null) {
+          + returnedClass().getCanonicalName() + "; " + value.getClass().getCanonicalName() + " is not supported");
+    } else if (value == null) {
       Hibernate.STRING.nullSafeSet(st, null, index, session);
       Hibernate.STRING.nullSafeSet(st, null, index + 1, session);
-    }
-    else {
-      OrganizationIdentifier id = (OrganizationIdentifier) value;
+    } else {
+      Identifier id = (Identifier) value;
       Hibernate.STRING.nullSafeSet(st, id.getClass().getCanonicalName(), index, session);
       Hibernate.STRING.nullSafeSet(st, id.getIdentifier(), index + 1, session);
     }
   }
 
   public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor session, Object owner)
-  throws HibernateException, SQLException {
+      throws HibernateException, SQLException {
     Identifier result = null;
     String clazz = null;
     String identifier = null;
     try {
-      clazz = (String)Hibernate.STRING.nullSafeGet(rs, names[0], session, owner);
-      identifier = (String)Hibernate.STRING.nullSafeGet(rs, names[1], session, owner);
+      clazz = (String) Hibernate.STRING.nullSafeGet(rs, names[0], session, owner);
+      identifier = (String) Hibernate.STRING.nullSafeGet(rs, names[1], session, owner);
       if (clazz == null && identifier == null) {
         result = null;
       } else {
@@ -116,15 +117,13 @@ public final class IdentifierCompositeUserType extends AbstractImmutableValueCom
         result = InstanceHelpers.robustNewInstance(idType, identifier);
       }
       return result;
-    }
-    catch (ArrayIndexOutOfBoundsException exc) {
+    } catch (ArrayIndexOutOfBoundsException exc) {
       throw new HibernateException("data received from database is not as expected: expected array of 2 values", exc);
-    }
-    catch (ClassCastException exc) {
+    } catch (ClassCastException exc) {
       throw new HibernateException("data received from database is not as expected: expected array of 2 values", exc);
-    }
-    catch (InvocationTargetException exc) {
-      throw new HibernateException("Creation of identifier of type " + clazz + " failed with an application exception", exc);
+    } catch (InvocationTargetException exc) {
+      throw new HibernateException("Creation of identifier of type " + clazz + " failed with an application exception",
+          exc);
     }
   }
 
