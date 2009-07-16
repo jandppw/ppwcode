72fcf4f96a64aa4d040b7cc44c470081a081ac35
Jurgen.Lust <Jurgen.Lust@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-07-16 10:17:10 +0000

    added archetype for ppwcode projects

---
+++ java/maven2/archetype/trunk/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.ppwcode.maven.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
