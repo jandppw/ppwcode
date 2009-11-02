8a558a166fa3ca21afe2681e6faa47c556945092
adriaan.peeters <adriaan.peeters@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-11-02 10:07:31 +0000

    Use local method instead of referring to MovieService

--- demo/FacebookDemo/trunk/src/main/java/be/peopleware/theopenmoviedb/Util.java
+++ demo/FacebookDemo/trunk/src/main/java/be/peopleware/theopenmoviedb/Util.java
@@ -17,7 +17,7 @@ public class Util {
 	}
 	
 	public static String getPosterThumb(String id) {
-		Movie movie = MovieService.searchForMovie(id);
+		Movie movie = getMovie(id);
 		if (movie != null && movie.getPosters().get("thumb") != null) {
 			return movie.getPosters().get("thumb");
 		} else {
@@ -26,7 +26,7 @@ public class Util {
 	}
 	
 	public static String getShortOverview(String id) {
-		Movie movie = MovieService.searchForMovie(id);
+		Movie movie = getMovie(id);
 		if (movie != null) {
 			return movie.getShortOverview();
 		} else {
@@ -35,7 +35,7 @@ public class Util {
 	}
 	
 	public static List<String> getMainActors(String id) {
-		Movie movie = MovieService.searchForMovie(id);
+		Movie movie = getMovie(id);
 		List<String> result = new ArrayList<String>();
 		if (movie != null) {
 			for (int i=0; i < 5 && i < movie.getActors().size(); i++) {
@@ -47,12 +47,12 @@ public class Util {
 	
 	public static void main(String[] args) {
 		String query = "the matrix";
-		for (Movie movie : MovieService.searchForMovies(query)) {
+		for (Movie movie : getMovies(query)) {
 			System.out.println(movie.getTitle() + " (" + movie.getImdbId() + ") : " + movie.getShortOverview());
 		}
 		
 		String queryId = "24";
-		Movie movie = MovieService.searchForMovie(queryId);
+		Movie movie = getMovie(queryId);
 		System.out.println(movie.getTitle() + " (" + movie.getImdbId() + ") : " + movie.getShortOverview());
 		System.out.println(movie.getActors());
 		for(String s : getMainActors(queryId)) {
