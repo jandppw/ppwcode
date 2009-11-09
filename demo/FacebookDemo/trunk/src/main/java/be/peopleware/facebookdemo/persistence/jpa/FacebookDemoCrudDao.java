1665aac2145d60e6a4c677f54b4b96952ab45f10
Tom.Mahieu <Tom.Mahieu@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-11-09 13:47:37 +0000

    Added roles for creation and updating + hardcoding roles in Application specific CRUDDao (for now...)

--- demo/FacebookDemo/trunk/src/main/java/be/peopleware/facebookdemo/persistence/jpa/FacebookDemoCrudDao.java
+++ demo/FacebookDemo/trunk/src/main/java/be/peopleware/facebookdemo/persistence/jpa/FacebookDemoCrudDao.java
@@ -2,7 +2,9 @@ package be.peopleware.facebookdemo.persistence.jpa;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.http.HttpServletRequest;
 
+import org.directwebremoting.WebContextFactory;
 import org.ppwcode.vernacular.exception_III.handle.ExceptionHandler;
 import org.ppwcode.vernacular.persistence_III.dao.jpa.JpaOutOfContainerStatelessCrudDao;
 
@@ -10,9 +12,27 @@ public class FacebookDemoCrudDao
 		extends
 		org.ppwcode.vernacular.persistence_III.dao.jpa.JpaRemoteAtomicStatelessCrudDao {
 
+	private static final String ANONYMOUS_ROLE_NAME = "anonymous";
+
 	public FacebookDemoCrudDao() {
 		setExceptionHandler(new ExceptionHandler());
-		setStatelessCrudJoinTransactionDao(new JpaOutOfContainerStatelessCrudDao());
+		setStatelessCrudJoinTransactionDao(new JpaOutOfContainerStatelessCrudDao(){
+
+			/**
+			 * This allows every user to perform a CRUD operation (the anonymous role
+			 * is defined for create and update on the Movie object.) 
+			 * 
+			 * TODO: a facebook connect session should result (preferrably transparently)
+			 * in the creation of a JAAS compatible security context.  This would enable
+			 * us to define the roles declaratively (in web.xml) instead of hard coding
+			 * them into the application.
+			 */
+			@Override
+			protected boolean isCallerInRole(String role) {
+				return ANONYMOUS_ROLE_NAME.equals(role);
+			}
+			
+		});
 	}
 	
 	EntityManagerFactory $entityManagerFactory = null;
