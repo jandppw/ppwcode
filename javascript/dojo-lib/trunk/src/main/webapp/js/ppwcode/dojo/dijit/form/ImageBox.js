0a940b0907d512a0f87413c1879791ecbfd3bb11
Tom.Mahieu <Tom.Mahieu@463e38ab-4f4e-0410-907a-65d2b3888964>
2009-11-09 13:45:11 +0000

    removed debugging output

--- javascript/dojo-lib/trunk/src/main/webapp/js/ppwcode/dojo/dijit/form/ImageBox.js
+++ javascript/dojo-lib/trunk/src/main/webapp/js/ppwcode/dojo/dijit/form/ImageBox.js
@@ -7,10 +7,6 @@ dojo.declare(
 	"ppwcode.dojo.dijit.form.ImageBox",
 	dijit.form._FormValueWidget,
 	{
-		constructor: function() {
-			console.log("Imagebox, blank Gif:" + this._blankGif);
-		},
-		
 		imgSrc: "",
 		
 		templateString: "<div dojoAttachPoint='focusNode'><img dojoAttachPoint='imgNode' src='${_blankGif}'></img></div>",
