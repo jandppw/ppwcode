a3b507de6eb2b664c16579116002370f7307049c
jandockx <jandockx@gmail.com>
2011-02-06 15:27:07 +0000

    Copy of other project, as setup of structure for SQLServer util code.

---
+++ dotnet/Util/SQLServer/trunk/I/NamespaceDoc.cs
@@ -0,0 +1,24 @@
+﻿#region Using
+
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace PPWCode.Util.SharePoint.I
+{
+    /// <summary>
+    /// <para>A library that wraps remote SharePoint
+    /// functionality, and adds some extra functionality.</para>
+    /// <para>The functionality is defined in <see cref="ISharePointClient"/>.</para>
+    /// <para><see cref="SharePointClient"/> implements the interface.
+    /// In a future version, this might better be moved to a separate
+    /// &quot;Impl&quot; assembly.</para>
+    /// <para><see cref="NopSharePointClient"/> mocks the functionality.
+    /// In a future version, this might better be move to a separate
+    /// &quot;Mock&quot; assembly.</para>
+    /// </summary>
+    [CompilerGenerated]
+    internal sealed class NamespaceDoc
+    {
+    }
+}
