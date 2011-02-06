d28e37d8f3fe48d84517b01e6c51fa5c35ab7a9c
jandockx@gmail.com <jandockx@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2011-02-06 16:48:42 +0000

    changed for this project

--- dotnet/Util/SQLServer/trunk/I/Properties/AssemblyInfo.cs
+++ dotnet/Util/SQLServer/trunk/I/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PPWCode.Util.SharePoint.I")]
-[assembly: AssemblyDescription("Main code of the PPWCode SharePoint Util package")]
+[assembly: AssemblyTitle("PPWCode.Util.SQLServer.I")]
+[assembly: AssemblyDescription("Main code of the PPWCode SQLServer Util package")]
 [assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -15,4 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a96a19e0-b3e8-4181-a2be-9518870d63d8")]
+[assembly: Guid("DE143347-7D42-48e5-BBA9-39EB2548F597")]
+
