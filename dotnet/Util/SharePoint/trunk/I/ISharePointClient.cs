ff3f869eb0b6936527997a538eabbe07af56fd4e
jandockx <jandockx@gmail.com>
2011-02-16 13:12:47 +0000

    Added code retrieved from Chenai work on Documents. SharePointDocument is needed now, was copied from an other project that is now obsolete. Added license headers. TODO's are done, except for release.

--- dotnet/Util/SharePoint/trunk/I/ISharePointClient.cs
+++ dotnet/Util/SharePoint/trunk/I/ISharePointClient.cs
@@ -1,4 +1,20 @@
-﻿#region Using
+/*
+ * Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Using
 
 using System.Diagnostics.Contracts;
 
@@ -32,6 +48,7 @@ namespace PPWCode.Util.SharePoint.I
         /// <param name="relativeUrl">The URL of a folder in a SharePoint document library,
         /// relative to <see cref="SharePointSiteUrl"/>.</param>
         void EnsureFolder(string relativeUrl);
+        void UploadDocument(string relativeUrl, SharePointDocument doc);
     }
 
     // ReSharper disable InconsistentNaming
@@ -58,6 +75,13 @@ namespace PPWCode.Util.SharePoint.I
             Contract.Requires(!relativeUrl.StartsWith(SharePointSiteUrl));
         }
 
+        public void UploadDocument(string relativeUrl, SharePointDocument doc)
+        {
+            Contract.Requires(SharePointSiteUrl != null && SharePointSiteUrl != string.Empty);
+            Contract.Requires(relativeUrl != null);
+            Contract.Requires(!relativeUrl.StartsWith(SharePointSiteUrl));
+            Contract.Requires(doc != null);
+        }
         #endregion
     }
 }
