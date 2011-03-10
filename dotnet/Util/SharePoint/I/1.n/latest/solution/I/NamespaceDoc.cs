eca3b56fbf8c7d8a563198acf6f58de814b63d70
jandockx@gmail.com <jandockx@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2011-03-10 18:38:31 +0000

    release I 1.0

---
+++ dotnet/Util/SharePoint/I/1.n/latest/solution/I/NamespaceDoc.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Using
+
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace PPWCode.Util.SharePoint.I
+{
+    /// <summary>
+    /// <para>A library that wraps remote SharePoint
+    /// functionality, and adds some extra functionality.</para>
+    /// <para>The functionality is defined in <see cref="ISharePointClient"/>.</para>
+    /// <para><see cref="SharePointClient"/> implements the interface.
+    /// In a future version, this might better be moved to a separate
+    /// &quot;Impl&quot; assembly.</para>
+    /// <para><see cref="NopSharePointClient"/> mocks the functionality.
+    /// In a future version, this might better be move to a separate
+    /// &quot;Mock&quot; assembly.</para>
+    /// </summary>
+    [CompilerGenerated]
+    internal sealed class NamespaceDoc
+    {
+    }
+}
