5d5b148ceed0a598fdc7442b0f4698f47a055cb4
jandockx@gmail.com <jandockx@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2011-03-10 15:44:12 +0000

    Added license header

--- dotnet/Kit/Tasks/trunk/Server.Mock/API_I/TasksDaoMock.cs
+++ dotnet/Kit/Tasks/trunk/Server.Mock/API_I/TasksDaoMock.cs
@@ -1,9 +1,25 @@
-﻿#region Using
+﻿/*
+ * Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Using
 
 using System;
 using System.Collections.Generic;
-using System.ServiceModel;
 using System.Linq;
+using System.ServiceModel;
 
 using PPWCode.Kit.Tasks.API_I;
 using PPWCode.Vernacular.Persistence.I;
@@ -119,7 +135,10 @@ namespace PPWCode.Kit.Tasks.Server.Mock.API_I
 
         public bool IsOperational
         {
-            get { return true; }
+            get
+            {
+                return true;
+            }
         }
 
         #endregion
@@ -208,7 +227,10 @@ namespace PPWCode.Kit.Tasks.Server.Mock.API_I
 
         public CommunicationState State
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                throw new NotImplementedException();
+            }
         }
 
 #pragma warning restore 67
