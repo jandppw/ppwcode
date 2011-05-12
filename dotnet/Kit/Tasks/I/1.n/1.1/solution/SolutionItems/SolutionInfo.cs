e838bc8c89fe50e083f681e5eb3b31c741d549c0
dvdwouwe <dvdwouwe@telenet.be>
2011-05-12 15:42:09 +0000

    * Better usage of AssemblyVersion / AssemblyFileVersion ==> avoid exceptions in VS using the winform designer that a specific assembly isn't found

---
+++ dotnet/Kit/Tasks/I/1.n/1.1/solution/SolutionItems/SolutionInfo.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Copyright 2004 - $Date: 2008-11-15 23:58:07 +0100 (za, 15 nov 2008) $ by PeopleWare n.v..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Using
+
+using System.Reflection;
+
+#endregion
+
+[assembly: AssemblyCompany("Peopleware NV")]
+[assembly: AssemblyProduct("PPWCode Kit Tasks")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.5035.0")]
