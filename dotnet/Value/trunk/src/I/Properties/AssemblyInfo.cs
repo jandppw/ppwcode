65896d659552a58f50e53f619cd0ffd684e7b913
jandockx <jandockx@gmail.com>
2011-12-03 19:41:02 +0000

    This is another project starting up.

--- dotnet/Value/trunk/src/I/Properties/AssemblyInfo.cs
+++ dotnet/Value/trunk/src/I/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 
 #endregion
 
-[assembly: AssemblyTitle("PPWCode.Util.OddsAndEnds.I")]
-[assembly: AssemblyDescription("Main code of the PPWCode OddsAndEnds Util library")]
+[assembly: AssemblyTitle("PPWCode.Value.I")]
+[assembly: AssemblyDescription("Main code of the PPWCode Value library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f6132d24-8001-4bcc-96b0-6901a610e5bc")]
--- dotnet/Value/trunk/src/Test_I/Properties/AssemblyInfo.cs
+++ dotnet/Value/trunk/src/Test_I/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 
 #endregion
 
-[assembly: AssemblyTitle("PPWCode.Vernacular.Exceptions.Test_I")]
-[assembly: AssemblyDescription("Test code of the PPWCode Exceptions Vernacular")]
+[assembly: AssemblyTitle("PPWCode.Value.Test_I")]
+[assembly: AssemblyDescription("Test code of the PPWCode Value")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8c52ee8f-d53d-42d9-b7ad-a1cf552e0006")]
