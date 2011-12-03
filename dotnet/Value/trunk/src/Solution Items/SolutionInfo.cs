344a136c410cd9f0d456a5195ea38dcd5355039a
jandockx <jandockx@gmail.com>
2011-12-03 18:53:15 +0000

    This is another project starting up.

--- dotnet/Value/trunk/src/Solution Items/SolutionInfo.cs
+++ dotnet/Value/trunk/src/Solution Items/SolutionInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("Peopleware NV")]
-[assembly: AssemblyProduct("PPWCode Util OddsAndEnds")]
-[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyProduct("PPWCode Value")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2011-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyFileVersion("1.7.5049.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
