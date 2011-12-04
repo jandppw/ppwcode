f5363603842e8f5e10b6465b4e720e5737c1f878
jandockx@gmail.com <jandockx@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2011-12-04 16:57:31 +0000

    demonstration of order operators behavior with null DateTime?

---
+++ dotnet/Value/trunk/src/I/Time/Interval/BeginEndTimeInterval.cs
@@ -0,0 +1,61 @@
+﻿/*<license>
+Copyright 2004 - $Date: 2008-12-07 22:15:22 +0100 (Sun, 07 Dec 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+#region Using
+
+using System;
+using System.Diagnostics.Contracts;
+
+#endregion
+
+namespace PPWCode.Value.I.Time.Interval
+{
+    /// <summary>
+    /// An actual time interval, that is constructed using a begin and an end date,
+    /// and has no further restrictions. It is not possible for both the begin and
+    /// the end date to be <c>null</c>.
+    /// </summary>
+    public sealed class BeginEndTimeInterval : AbstractBeginEndTimeInterval
+    {
+        public BeginEndTimeInterval(DateTime? begin, DateTime? end)
+            : base(begin, end)
+        {
+            Contract.Ensures(Begin == begin);
+            Contract.Ensures(End == end);
+            Contract.EnsuresOnThrow<IllegalTimeIntervalException>(
+                begin == null && end == null
+                || begin != null && end != null && begin.Value > end.Value);
+
+            // NOP
+        }
+
+        public override ITimeInterval Determinate(DateTime? stubBegin, DateTime? stubEnd)
+        {
+            return DeterminateBeginEndTimeInterval(stubBegin, stubEnd);
+        }
+
+        public BeginEndTimeInterval DeterminateBeginEndTimeInterval(DateTime? stubBegin, DateTime? stubEnd)
+        {
+            Contract.Ensures(Contract.Result<BeginEndTimeInterval>() != null);
+            Contract.Ensures((Contract.Result<BeginEndTimeInterval>().Begin == DeterminateBegin(stubBegin)));
+            Contract.Ensures(Contract.Result<BeginEndTimeInterval>().End == DeterminateEnd(stubEnd));
+
+            Contract.EnsuresOnThrow<IllegalTimeIntervalException>(DeterminateBegin(stubBegin) > DeterminateEnd(stubEnd));
+
+            return new BeginEndTimeInterval(DeterminateBegin(stubBegin), DeterminateEnd(stubEnd));
+        }
+    }
+}
---
+++ dotnet/Value/trunk/src/Test_I/Time/DateTimeBehaviorTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PPWCode.Value.Test_I.Time
+{
+    /// <summary>
+    /// Summary description for DateTimeBehaviorTest
+    /// </summary>
+    [TestClass]
+    public class DateTimeBehaviorTest
+    {
+        [TestMethod]
+        public void TestRelations1()
+        {
+            DateTime dt1 = new DateTime(2004, 7, 26);
+            DateTime dt2 = new DateTime(2007, 3, 6);
+            DateTime dt1Too = dt1;
+            Assert.IsTrue(dt1 < dt2);
+            Assert.IsTrue(dt1 <= dt2);
+            Assert.IsTrue(dt1 != dt2);
+            Assert.IsFalse(dt1 > dt2);
+            Assert.IsFalse(dt1 >= dt2);
+            Assert.IsFalse(dt1 == dt2);
+            Assert.IsFalse(dt1 < dt1Too);
+            Assert.IsTrue(dt1 <= dt1Too);
+            Assert.IsFalse(dt1 != dt1Too);
+            Assert.IsFalse(dt1 > dt1Too);
+            Assert.IsTrue(dt1 >= dt1Too);
+            Assert.IsTrue(dt1 == dt1Too);
+        }
+
+        [TestMethod]
+        public void TestRelations2()
+        {
+            DateTime? dt1 = new DateTime(2004, 7, 26);
+            DateTime? dt2 = new DateTime(2007, 3, 6);
+            TestRelations(dt1, dt2);
+        }
+
+        [TestMethod]
+        public void TestRelations3()
+        {
+            DateTime? dt2 = new DateTime(2007, 3, 6);
+            TestRelations(null, dt2);
+        }
+
+        [TestMethod]
+        public void TestRelations4()
+        {
+            DateTime? dt1 = new DateTime(2004, 7, 26);
+            TestRelations(dt1, null);
+        }
+
+        /// <summary>
+        /// DateTime? and order > is not total
+        /// </summary>
+        [TestMethod]
+        public void TestRelations5()
+        {
+            DateTime? dt1 = new DateTime(2004, 7, 26);
+            DateTime? dt2 = null;
+            Assert.IsFalse(dt1 < dt2);
+            Assert.IsFalse(dt1 <= dt2);
+            Assert.IsFalse(dt1 == dt2);
+            Assert.IsFalse(dt1 > dt2);
+            Assert.IsFalse(dt1 >= dt2);
+        }
+
+        public void TestRelations(DateTime? dt1, DateTime? dt2)
+        {
+            DateTime? dt1Too = dt1;
+            Assert.AreEqual(dt1 < dt2, dt1 != null && dt2 != null && dt1.Value < dt2.Value);
+            Assert.AreEqual(dt1 <= dt2, dt1 != null && dt2 != null && dt1.Value <= dt2.Value);
+            Assert.IsTrue(dt1 != dt2);
+            Assert.AreEqual(dt1 > dt2, dt1 != null && dt2 != null && dt1.Value > dt2.Value);
+            Assert.AreEqual(dt1 >= dt2, dt1 != null && dt2 != null && dt1.Value >= dt2.Value);
+            Assert.IsFalse(dt1 == dt2);
+            Assert.IsFalse(dt1 < dt1Too);
+            Assert.AreEqual(dt1 <= dt1Too, dt1 != null);
+            Assert.IsFalse(dt1 != dt1Too);
+            Assert.IsFalse(dt1 > dt1Too);
+            Assert.AreEqual(dt1 >= dt1Too, dt1 != null);
+            Assert.IsTrue(dt1 == dt1Too);
+        }
+    }
+}
