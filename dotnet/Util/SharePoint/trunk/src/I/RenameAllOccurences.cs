42dd4589a926d3c31d6e0316aec1c4903b9385a3
KristelBogaerts1 <KristelBogaerts1@gmail.com>
2012-01-04 15:38:15 +0000

    RenameAllOccurences

---
+++ dotnet/Util/SharePoint/trunk/src/I/RenameAllOccurences.cs
@@ -0,0 +1,30 @@
+﻿namespace PPWCode.Util.SharePoint.I
+{
+    public class RenameAllOccurences : ISharepointAction
+    {
+        public RenameAllOccurences(ISharePointClient iSharePointClient, string baseRelativeUrl, string oldFolderName, string newFolderName)
+        {
+            Sharepoint = iSharePointClient;
+            BaseRelativeUrl = baseRelativeUrl;
+            OldFolderName = oldFolderName;
+            NewFolderName = newFolderName;
+        }
+
+        public void Execute()
+        {
+            // MUDO: rename all occurrences
+            Sharepoint.RenameFolder(BaseRelativeUrl, OldFolderName, NewFolderName);
+        }
+
+        public void Undo()
+        {
+            // MUDO: rename all occurrences
+            Sharepoint.RenameFolder(BaseRelativeUrl, NewFolderName, OldFolderName);
+        }
+
+        public ISharePointClient Sharepoint { get; set; }
+        public string BaseRelativeUrl { get; set; }
+        public string OldFolderName { get; set; }
+        public string NewFolderName { get; set; }
+    }
+}
