5cf8128a1d8a4603f6f06ac142a5dfe5649aa9dc
KristelBogaerts1@gmail.com <KristelBogaerts1@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2012-01-04 15:49:37 +0000

    SEMPERA-2149 add class RenameFolder and RenameAllOccurrences              add interface ISharepointAction

--- dotnet/Util/SharePoint/trunk/src/I/RenameFolder.cs
+++ dotnet/Util/SharePoint/trunk/src/I/RenameFolder.cs
@@ -1,30 +1,32 @@
-﻿namespace PPWCode.Util.SharePoint.I
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PPWCode.Util.SharePoint.I
 {
-    public class RenameAllOccurences : ISharepointAction
+    public class RenameFolder : ISharepointAction
     {
-        public RenameAllOccurences(ISharePointClient iSharePointClient, string baseRelativeUrl, string oldFolderName, string newFolderName)
+        public RenameFolder(ISharePointClient iSharePointClient, string baseRelativeUrl, string oldFolderName, string newFolderName)
         {
             Sharepoint = iSharePointClient;
             BaseRelativeUrl = baseRelativeUrl;
             OldFolderName = oldFolderName;
             NewFolderName = newFolderName;
         }
-
         public void Do()
         {
-            // MUDO: rename all occurrences
-            Sharepoint.RenameFolder(BaseRelativeUrl, OldFolderName, NewFolderName);
+           Sharepoint.RenameFolder(BaseRelativeUrl, OldFolderName, NewFolderName);
         }
 
         public void Undo()
         {
-            // MUDO: rename all occurrences
             Sharepoint.RenameFolder(BaseRelativeUrl, NewFolderName, OldFolderName);
         }
-
         public ISharePointClient Sharepoint { get; set; }
         public string BaseRelativeUrl { get; set; }
         public string OldFolderName { get; set; }
         public string NewFolderName { get; set; }
     }
+      
 }
