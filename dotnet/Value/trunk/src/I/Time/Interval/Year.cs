1409ddf78efb3c3408695374aff217e8fa7a0682
jandockx <jandockx@gmail.com>
2012-02-19 00:06:40 +0000

    Introduced Year TimeInterval with tests. Tests don't run on this computer, sadly, due to config error with Contracts, I can't solve quickly on my own.

---
+++ dotnet/Value/trunk/src/I/Time/Interval/Year.cs
@@ -0,0 +1,127 @@
+﻿/*<license>
+Copyright 2004 - $Date: 2008-12-07 22:15:22 +0100 (Sun, 07 Dec 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+#region Using
+
+using System;
+using System.Diagnostics.Contracts;
+
+#endregion
+
+namespace PPWCode.Value.I.Time.Interval
+{
+    /// <summary>
+    /// An actual year, according to the Gregorian calendar.
+    /// </summary>
+    public sealed class Year : AbstractTimeInterval
+    {
+        #region Type invariants
+
+        [ContractInvariantMethod]
+        private void TypeInvariants()
+        {
+            Contract.Invariant(Begin == new DateTime(Int, 1, 1));
+            Contract.Invariant(End == new DateTime(Int + 1, 1, 1));
+            Contract.Invariant(Duration == (IsLeapYear ? DURATION_LEAP_YEAR : DURATION_NON_LEAP_YEAR));
+        }
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Create a new year instance from an integer representing the year number.
+        /// </summary>
+        /// <param name="int">The year, as a number, in the Gregorian calendar.
+        /// There are no limitations.</param>
+        public Year(int @int)
+        {
+            Contract.Ensures(Int == @int);
+
+            m_Int = @int;
+            m_Begin = new DateTime(@int, 1, 1);
+            m_End = new DateTime(@int + 1, 1, 1);
+        }
+
+        /// <summary>
+        /// Create a new year instance, representing the year in which a given
+        /// <see cref="DateTime"/> falls.
+        /// </summary>
+        /// <param name="dt">This date falls in the resulting year instance</param>
+        public Year(DateTime dt) : this(dt.Year)
+        {
+            Contract.Ensures(TimePointIntervalRelation.LeastUncertainTimePointIntervalRelation(dt, this).Equals(TimePointIntervalRelation.In));
+            
+            // NOP
+        }
+
+        #endregion
+
+        #region Constants
+
+        public static readonly TimeSpan DURATION_NON_LEAP_YEAR = new TimeSpan(365, 0, 0, 0);
+        public static readonly TimeSpan DURATION_LEAP_YEAR = new TimeSpan(366, 0, 0, 0);
+
+        #endregion
+
+        #region Properties
+
+        private readonly int m_Int;
+
+        public int Int
+        {
+            get { return m_Int; }
+        }
+
+        public bool IsLeapYear
+        {
+            get
+            {
+                Contract.Ensures(Contract.Result<bool>() == DateTime.IsLeapYear(Int));
+
+                return DateTime.IsLeapYear(m_Int);
+            }
+        }
+
+        private readonly DateTime m_Begin;
+
+        public override DateTime? Begin
+        {
+            get { return m_Begin; }
+        }
+
+        private readonly DateTime m_End;
+
+        public override DateTime? End
+        {
+            get { return m_End; }
+        }
+
+        public override TimeSpan? Duration
+        {
+            get { return IsLeapYear ? DURATION_LEAP_YEAR : DURATION_NON_LEAP_YEAR; }
+        }
+
+        public override ITimeInterval Determinate(DateTime? stubBegin, DateTime? stubEnd)
+        {
+            Contract.Ensures((Year) Contract.Result<ITimeInterval>() == this);
+
+            return this;
+        }
+
+        #endregion
+    }
+}
---
+++ dotnet/Value/trunk/src/Test_I/Time/Interval/YearTest.cs
@@ -0,0 +1,154 @@
+﻿/*<license>
+Copyright 2011 - $Date: 2008-11-06 15:27:53 +0100 (Thu, 06 Nov 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PPWCode.Value.I.Time.Interval;
+
+#endregion
+
+namespace PPWCode.Value.Test_I.Time.Interval
+{
+    [TestClass]
+    public class YearTest : AbstractTimeIntervalTest
+    {
+        #region Additional test attributes
+
+        private int[] m_IntSubjects;
+
+
+        public IList<Year> YearSubjects
+        {
+            get
+            {
+                return Subjects.Cast<Year>().ToList();
+            }
+        }
+
+        private DateTime?[] m_Dates;
+
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            InitSubjects();
+
+            m_IntSubjects = new int[]
+                {
+                    Int32.MinValue, -234544, -2346, -123, -4,
+                    0,
+                    1, 6, 33, 346, 1256, 1856, 1914, 1999, 2000, 2001, 2011, 2012,
+                    2013, 2145, 2234, 43566, 434534343, Int32.MaxValue
+                };
+
+            foreach (int i in m_IntSubjects)
+            {
+                AddSubject(new Year(i));
+            }
+
+            m_Dates = new DateTime?[]
+            {
+                new DateTime(Int32.MinValue, 4, 6),
+                new DateTime(-345, 2, 12),
+                new DateTime(0, 5, 6), 
+                new DateTime(1999, 12, 31),
+                DateTime.Now,
+                DateTime.UtcNow,
+                new DateTime(2011, 1, 1, 0, 0, 0, 0), 
+                new DateTime(2011, 12, 31, 23, 59, 59, 999), 
+                new DateTime(2011, 2, 19, 14, 35, 34, 345, DateTimeKind.Utc), 
+                new DateTime(2011, 2, 28), 
+                new DateTime(2012, 1, 1, 0, 0, 0, 0), 
+                new DateTime(2012, 12, 31, 23, 59, 59, 999), 
+                new DateTime(2012, 2, 19, 14, 35, 34, 345, DateTimeKind.Utc), 
+                new DateTime(2012, 2, 28), 
+                new DateTime(2012, 2, 29), 
+                new DateTime(Int32.MaxValue, 12, 31, 23, 59, 59, 999)
+            };
+        }
+
+        [TestCleanup]
+        public override void TestCleanup()
+        {
+            base.TestCleanup();
+            m_IntSubjects = null;
+            m_Dates = null;
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void TestDeterminate()
+        {
+            ITimeInterval result = null;
+            foreach (BeginEndTimeInterval subject in Subjects)
+            {
+                try
+                {
+                    result = subject.Determinate(null, null);
+                }
+                catch (IllegalTimeIntervalException)
+                {
+                    // NOP
+                }
+                foreach (DateTime? dt1 in m_Dates)
+                {
+                    try
+                    {
+                        result = subject.Determinate(dt1, null);
+                    }
+                    catch (IllegalTimeIntervalException)
+                    {
+                        // NOP
+                    }
+                    try
+                    {
+                        result = subject.Determinate(null, dt1);
+                    }
+                    catch (IllegalTimeIntervalException)
+                    {
+                        // NOP
+                    }
+                    foreach (DateTime? dt2 in m_Dates)
+                    {
+                        try
+                        {
+                            result = subject.Determinate(dt1, dt2);
+                        }
+                        catch (IllegalTimeIntervalException)
+                        {
+                            // NOP
+                        }
+                    }                    
+                }                
+            }
+        }
+
+        [TestMethod]
+        public void TestConstructor_Int()
+        {
+            foreach (int i in m_IntSubjects)
+            {
+                Year result = new Year(i);
+            }
+        }
+    }
+}
