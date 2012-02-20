018cbe2d2ef9c36ff7d1a630118ef406797f4e56
ruben.vandeginste@gmail.com <ruben.vandeginste@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2012-02-20 15:04:14 +0000

    SEMPERA-1518 - upping dependency of OddsAndEnds to 2.0

--- dotnet/Util/SharePoint/trunk/src/Solution Items/SolutionInfo.cs
+++ dotnet/Util/SharePoint/trunk/src/Solution Items/SolutionInfo.cs
@@ -5,5 +5,9 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// assembly version
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+
+// keep in sync with package version
+[assembly: AssemblyFileVersion("2.2.1.0")]
