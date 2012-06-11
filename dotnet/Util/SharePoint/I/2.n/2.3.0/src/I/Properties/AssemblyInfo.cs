db02d26b456b12e7d8150d416396a1a2425e3ea1
ruben.vandeginste@gmail.com <ruben.vandeginste@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2012-06-11 16:06:32 +0000

    SEMPERA-2400 - release 2.3.0

---
+++ dotnet/Util/SharePoint/I/2.n/2.3.0/src/I/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿#region Using
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("PPWCode.Util.SharePoint.I")]
+[assembly: AssemblyDescription("Main code of the PPWCode SharePoint Util package")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a96a19e0-b3e8-4181-a2be-9518870d63d8")]
---
+++ dotnet/Util/SharePoint/I/2.n/2.3.0/src/Solution Items/SolutionInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Peopleware NV")]
+[assembly: AssemblyProduct("PPWCode Util SharePoint")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// assembly version
+[assembly: AssemblyVersion("2.0.0.0")]
+
+// keep in sync with package version
+[assembly: AssemblyFileVersion("2.3.0.0")]
