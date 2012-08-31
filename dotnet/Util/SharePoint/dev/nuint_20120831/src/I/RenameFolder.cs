e8e6fc7f0c04f794803da28b103d11b2063a82b2
KristelBogaerts1@gmail.com <KristelBogaerts1@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2012-08-31 08:42:37 +0000

    testen aanpassen voor gebruik op Jenkins

---
+++ dotnet/Util/SharePoint/dev/nuint_20120831/src/I/RenameFolder.cs
@@ -0,0 +1,31 @@
+﻿namespace PPWCode.Util.SharePoint.I
+{
+    public class RenameFolder : ISharepointAction
+    {
+        public RenameFolder(ISharePointClient iSharePointClient, string baseRelativeUrl, string oldFolderName, string newFolderName)
+        {
+            Sharepoint = iSharePointClient;
+            BaseRelativeUrl = baseRelativeUrl;
+            OldFolderName = oldFolderName;
+            NewFolderName = newFolderName;
+        }
+
+        public void Do()
+        {
+            Sharepoint.RenameFolder(BaseRelativeUrl, OldFolderName, NewFolderName);
+        }
+
+        public void Undo()
+        {
+            Sharepoint.RenameFolder(BaseRelativeUrl, NewFolderName, OldFolderName);
+        }
+
+        public ISharePointClient Sharepoint { get; set; }
+
+        public string BaseRelativeUrl { get; set; }
+
+        public string OldFolderName { get; set; }
+
+        public string NewFolderName { get; set; }
+    }
+}
