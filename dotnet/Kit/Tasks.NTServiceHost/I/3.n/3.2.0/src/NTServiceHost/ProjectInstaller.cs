4be74b97d5de35353813805f9bacbe758811c824
ruben.vandeginste <ruben.vandeginste@gmail.com>
2012-09-06 15:50:25 +0000

    Release 3.2.0 PPWCode.Kit.Tasks.NTServiceHost

---
+++ dotnet/Kit/Tasks.NTServiceHost/I/3.n/3.2.0/src/NTServiceHost/ProjectInstaller.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace PPWCode.Kit.Tasks.NTServiceHost
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
