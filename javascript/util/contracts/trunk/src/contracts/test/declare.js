7c519114f01e535a5c3d26d4edfb7b878ef15fb7
jandockx <jandockx@gmail.com>
2012-10-19 19:42:28 +0000

    first test runs

--- javascript/util/contracts/trunk/src/contracts/test/declare.js
+++ javascript/util/contracts/trunk/src/contracts/test/declare.js
@@ -1,23 +1,10 @@
-define(["dojo/main", "util/doh", "be/ppwcode/util/contracts/I/declare"],
+define(["dojo/main", "util/doh/main", "contracts/declare"],
   function(dojo, doh, subjectDeclare) {
 
     doh.register("be/ppwcode/util/contracts/I/declare", [
 
-      function testConstructor() {
-        var idStub = "ID stub";
-        var subject = new Loggable({id: idStub});
-        doh.invars(subject);
-        // post
-        doh.is(idStub, subject.id);
-      },
-
-      function testToString() {
-        var subject = new Loggable({id: "test"});
-        var result = subject.toString();
-        doh.isNot(null, result);
-        doh.t(typeof result === "string");
-        doh.isNot("", result);
-        console.log(result);
+      function testDoh() {
+        console.log("test ran");
       }
 
     ]);
