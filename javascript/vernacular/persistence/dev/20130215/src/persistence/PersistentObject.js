a59e9ee8b3e1e4ac2225efa9a124f3dfc7d56d04
jandockx <jandockx@gmail.com>
2013-02-18 07:11:20 +0000

    Tag what was used in demo Friday.

---
+++ javascript/vernacular/persistence/dev/20130215/src/persistence/PersistentObject.js
@@ -0,0 +1,60 @@
+define(["dojo/_base/declare", "ppwcode/semantics/SemanticObject"],
+  function(declare, SemanticObject) {
+
+    function internalReload(/*PersistentObject*/ self, /*Object*/ json) {
+      if (json && json.persistenceId /* TODO json.persistenceId undefined, but not null */) {
+        if (self.persistenceId && json.persistenceId != null && json.persistenceId != self.persistenceId) {
+          throw "ERROR cannot change from an existing persistenceId to another (" +
+            json.toString() + ")"; // MUDO better error, precondition
+        }
+        // this will happen with the JSON response from a creation or IdNotFoundException, and during construction
+        //noinspection JSUnresolvedFunction
+        self._changeAttrValue("persistenceId", json.persistenceId);
+      }
+    }
+
+    var PersistentObject = declare("be.ppwcode.vernacular.persistence.PersistentObject",
+                                   [SemanticObject], {
+
+      _c_invar: [
+        function() {return this.hasOwnProperty("persistenceId");}
+        /* we don't care about the format of the persistenceId here; we just keep it, and return it to the server
+         like we got it. */
+      ],
+
+      constructor: function(/*Object*/ props) {
+        /* we don't care about the format of the persistenceId here; we just keep it, and return it to the server
+         like we got it. */
+
+        this.persistenceId = null; // init for when there are no props
+        internalReload(this, props);
+      },
+
+      reload: function(/*Object*/ json) {
+        // persistenceId can change from null to an actual number
+        internalReload(this, json);
+      },
+
+/*
+      _persistenceIdGetter: function() {
+        return this.persistenceId;
+      },
+*/
+
+      _persistenceIdSetter: function() {
+        // persistenceId is read-only
+        throw "error"; // MUDO Make this a special kind of Error (ppwcode exceptions)
+      },
+
+      _extendJsonObject: function(/*Object*/ json) {
+        json.persistenceId = this.persistenceId;
+      },
+
+      _stateToString: function(/*Array of String*/ toStrings) {
+        toStrings.push("persistenceId: " + this.persistenceId);
+      }
+    });
+
+    return PersistentObject; // return PersistentObject
+  }
+);
