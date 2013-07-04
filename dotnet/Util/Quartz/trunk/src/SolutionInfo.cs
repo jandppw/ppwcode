8c1b8b6509f7040ee8585bf7cefe654b47b23f18
ruben.vandeginste <ruben.vandeginste@gmail.com>
2013-07-04 15:50:45 +0000

    SEMPERA-2807 Cleanup warnings.

--- dotnet/Util/Quartz/trunk/src/SolutionInfo.cs
+++ dotnet/Util/Quartz/trunk/src/SolutionInfo.cs
@@ -2,12 +2,12 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Peopleware NV")]
 [assembly: AssemblyProduct("PPWCode Util Quartz")]
-[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // assembly version
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 // keep in sync with package version
 [assembly: AssemblyFileVersion("1.0.0.0")]
