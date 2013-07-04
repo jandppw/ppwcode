43b30651ecd696c055fa8df98c096b5750c0f081
ruben.vandeginste@gmail.com <ruben.vandeginste@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2013-07-04 15:34:43 +0000

    SEMPERA-2807 Fix Initial commit.

---
+++ dotnet/Util/Quartz/trunk/src/I/AggregateException.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace PPWCode.Util.Quartz
+{
+    [Serializable]
+    public sealed class AggregateException : 
+        Exception
+    {
+         #region Constructors
+
+        public AggregateException()
+            : base(null, null)
+        {
+            Set = new HashSet<Exception>();
+        }
+
+        public AggregateException(string message)
+            : base(message)
+        {
+            Set = new HashSet<Exception>();
+        }
+
+        public AggregateException(string message, IEnumerable<Exception> exceptions)
+            :base(message)
+        {
+            Set = (HashSet<Exception>)exceptions;
+        }
+
+        private AggregateException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        #endregion
+
+        #region Properties
+     
+        private HashSet<Exception> Set
+        {
+            get
+            {
+                return Data["Set"] as HashSet<Exception>;
+            }
+            set
+            {
+                Data["Set"] = value;
+            }
+        }
+
+        public ICollection<Exception> Elements
+        {
+            get { return Set.ToArray(); }
+        }
+
+        #endregion
+
+        public void AddElement(Exception exception)
+        {
+            AggregateException cse = exception as AggregateException;
+            if (cse == null)
+            {
+                Set.Add(exception);
+            }
+            else
+            {
+                foreach (Exception ex in cse.Elements)
+                {
+                    AddElement(ex);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder(1024);
+                foreach (Exception se in Set)
+                {
+                    sb.AppendLine(se.ToString());
+                }
+                return sb.Length == 0 ? Message : sb.ToString();
+            }
+            catch
+            {
+                return Message;
+            }
+        }
+    }
+}
---
+++ dotnet/Util/Quartz/trunk/src/I/ConfigHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+
+namespace PPWCode.Util.Quartz
+{
+    public static class ConfigHelper
+    {
+        public static T GetAppSetting<T>(string key, T defaultValue)
+           where T : IConvertible
+        {
+            T result;
+            try
+            {
+                result = ConfigurationManager.AppSettings.Get(key) != null
+                             ? (T)Convert.ChangeType(ConfigurationManager.AppSettings[key], typeof(T), CultureInfo.InvariantCulture)
+                             : defaultValue;
+            }
+            catch
+            {
+                result = defaultValue;
+            }
+            return result;
+        }
+
+        public static string GetConnectionString(string key)
+        {
+            ConnectionStringSettings connectionString = ConfigurationManager.ConnectionStrings[key];
+            return connectionString == null ? null : connectionString.ConnectionString;
+        }
+    }
+}
