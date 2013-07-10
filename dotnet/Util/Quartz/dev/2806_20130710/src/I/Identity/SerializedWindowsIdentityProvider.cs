2aacdb4fa0c306085e494cba87d888f6fc21168c
kristel.bogaerts1 <kristel.bogaerts1@gmail.com>
2013-07-10 14:31:16 +0000

    SEMPERA-2806 upgrade naar visual studio 2012

---
+++ dotnet/Util/Quartz/dev/2806_20130710/src/I/Identity/SerializedWindowsIdentityProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Security.Principal;
+
+using PPWCode.Util.OddsAndEnds.I.ActiveDirectory;
+
+using Quartz;
+
+namespace PPWCode.Util.Quartz.I.Identity
+{
+    public class SerializedWindowsIdentityProvider : IIdentityProvider
+    {
+       private const string KeyUserAccount = @"KeyUserAccount";
+
+       public WindowsIdentity GetWindowsIdentity(JobExecutionContext context)
+       {
+            string userPrincipalName = null;
+            string userAccount = null;
+            JobDataMap jobDataMap = context.MergedJobDataMap;
+            if (jobDataMap.Contains(KeyUserAccount))
+            {
+                userAccount = Convert.ToString(jobDataMap[KeyUserAccount]);
+            }
+            if (String.IsNullOrEmpty(userAccount) || userAccount.Trim().Length == 0)
+            {
+                return WindowsIdentity.GetCurrent();
+            }
+            string domainName = AdSearch.GetDomainFromUserAccount(userAccount);
+            AdSearch adSearch = new AdSearch(domainName);
+            ResultPropertyValueCollection property = adSearch.GetProperty(userAccount, @"userprincipalname");
+            if (property != null && property.Count == 1)
+            {
+                userPrincipalName = property[0].ToString();
+            }
+            return userPrincipalName == null
+                    ? WindowsIdentity.GetCurrent()
+                    : new WindowsIdentity(userPrincipalName);
+            }
+        }
+    }
