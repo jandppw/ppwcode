3e97566e8672a7dddc77d210a8c827d4a1d8f012
kristel.bogaerts1@gmail.com <kristel.bogaerts1@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2013-07-12 12:58:37 +0000

    SEMPERA-2806 upgrade naar visual studio 2012

---
+++ dotnet/Vernacular/Exceptions/dev/2806_20130712/src/Solution Items/SolutionInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Peopleware NV")]
+[assembly: AssemblyProduct("PPWCode Vernacular Exceptions")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
