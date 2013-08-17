207629cd811d12d27346df6de17b39dff3e9073c
ruben.vandeginste <ruben.vandeginste@gmail.com>
2013-08-17 11:22:32 +0000

    SEMPERA-2846 Release PPWCode.Util.SharePoint 2.4.0

---
+++ dotnet/Util/SharePoint/I/2.n/2.4.0/src/I/SharePointDocumentversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PPWCode.Util.SharePoint.I
+{
+    public class SharePointDocumentVersion
+    {
+        public string Version { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Url { get; set; }
+
+        public SharePointDocumentVersion(string versionnr, DateTime creationDate, string url)
+        {
+            Version = versionnr;
+            CreationDate = creationDate;
+            Url = url;
+        }
+    }
+}
