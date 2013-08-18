3882ec715099df61b67c77f3913e57609f29ee50
ruben.vandeginste <ruben.vandeginste@gmail.com>
2013-08-18 23:46:42 +0000

    SEMPERA-2846 Bumping version.

--- dotnet/Kit/Tasks.Server/trunk/src/SolutionItems/SolutionInfo.cs
+++ dotnet/Kit/Tasks.Server/trunk/src/SolutionItems/SolutionInfo.cs
@@ -31,4 +31,4 @@ using System.Reflection;
 // version used for informational purposes
 // for bugfixes, this version number should be increased
 // and a tag should be made in subversion
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
