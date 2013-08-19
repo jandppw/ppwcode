20d31a1d0fa57fb60e1faf0fc6a64df77b67126c
ruben.vandeginste@gmail.com <ruben.vandeginste@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2013-08-19 15:04:51 +0000

    SEMPERA-2846 Release PPWCode.Util.Quartz 1.0.0.

---
+++ dotnet/Util/Quartz/I/1.n/1.0.0/src/I/Identity/IIdentityProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Principal;
+
+using Quartz;
+
+namespace PPWCode.Util.Quartz.I.Identity
+{
+    public interface IIdentityProvider
+    {
+        WindowsIdentity GetWindowsIdentity(JobExecutionContext rContext);
+    }
+}
