3efe94fe997202b6b4103f8441b6f7e78f357bd8
ruben.vandeginste@gmail.com <ruben.vandeginste@gmail.com@463e38ab-4f4e-0410-907a-65d2b3888964>
2014-02-28 13:39:52 +0000

    Release PPWCode.Utils.OddsAndEnds 2.3.1.

---
+++ dotnet/Util/OddsAndEnds/I/2.n/2.3.1/src/I/SpreadSheet/GenerateUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace PPWCode.Util.OddsAndEnds.I.SpreadSheet
+{
+    public class GenerateUtil
+    {
+        public static IList<T> ReadSheet<T>(string xlsFile, string sheet, List<string> columns, Func<DbDataReader, T>spreadSheetRowResolver)
+             where T : class 
+        {
+            ExcelUtil excelUtil = new ExcelUtil(xlsFile);
+            StringBuilder selectStatement = new StringBuilder();
+            selectStatement.Append(@"select");           
+            foreach (string item in columns)
+            {
+                selectStatement.Append("[" + item + "],");
+            }
+            selectStatement.Remove(selectStatement.Length - 1, 1);
+            selectStatement.Append(string.Format(@"from [{0}$]", sheet));
+
+            try
+            {
+                return excelUtil.ReadSheet<T>(selectStatement.ToString(), spreadSheetRowResolver);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(string.Format("Structure error in sheet: {0} with message : {1}", sheet, e.Message));
+            }
+        }
+    }
+}
