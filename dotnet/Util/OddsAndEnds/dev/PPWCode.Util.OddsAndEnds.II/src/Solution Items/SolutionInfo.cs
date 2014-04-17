f3b82a6c70cb17e7f1f0e49098f0176e8fe82528
ruben.vandeginste <ruben.vandeginste@gmail.com>
2014-04-17 22:31:32 +0000

    Branch for PPWCode.Util.OddsAndEnds mark version II

---
+++ dotnet/Util/OddsAndEnds/dev/PPWCode.Util.OddsAndEnds.II/src/Solution Items/SolutionInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Peopleware NV")]
+[assembly: AssemblyProduct("PPWCode Util OddsAndEnds")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.3.1.0")]
+[assembly: AssemblyInformationalVersion("2.3.1.0")]
